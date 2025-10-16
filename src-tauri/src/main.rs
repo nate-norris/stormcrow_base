ab687584235a20997deed18177f946d084546fe6
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2025-10-16 11:41:42 -0700

    possible usage of AppHandle

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// use tauri::{AppHandle, Manager};
+
+
 fn main() {
     chinook_lib::run()
 }
