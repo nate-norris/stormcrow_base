faac113563bd30553bc1daf2172e372d31db7c9e
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-02-24 13:19:27 -0800

    added state for mm2t and speaker

--- src-tauri/src/t_state.rs
+++ src-tauri/src/t_state.rs
@@ -4,14 +4,13 @@
 use std::sync::Arc;
 
 use crate::lib_sqlx::DbPool;
-// use crate::lib_mm2t::MM2TTx;
+use utils::speaker::SpeakerTx;
+use utils::mm2t::MM2TTransport;
 
 // database state
 pub struct DbState(pub Arc<DbPool>);
 
 // mm2t serial state
-// pub struct MM2TState {
-//     pub writer_tx: MM2TTx,         // send data from client
-// }
+pub struct MM2TState(pub Arc<MM2TTransport>);
 
-
+pub struct SpeakerState(pub Arc<SpeakerTx>);
