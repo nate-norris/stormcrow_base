37788e067283b6dc5a01670e97fc397f85a9ece3
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-02 07:47:34 -0800

    made speaker_tx static reference

--- src-tauri/src/mm2t_read/config.rs
+++ src-tauri/src/mm2t_read/config.rs
@@ -17,8 +17,13 @@ pub async fn init_mm2t(speaker_tx: &SpeakerTx) -> Option<MM2TTransport> {
 
 pub fn spawn_mm2t_read(mm2t: MM2TTransport, app_handle: AppHandle, 
     speaker_tx: &SpeakerTx) {
+    // create static speaker reference
+    let speaker_tx = speaker_tx.clone();
+
+    // spawn the read task
     tauri::async_runtime::spawn(async move {
         let mut decoder = PacketDecoder::new();
+        // 
         loop {
             match mm2t.read().await {
                 Ok(Some(byte)) => {
@@ -30,14 +35,10 @@ pub fn spawn_mm2t_read(mm2t: MM2TTransport, app_handle: AppHandle,
                         }
                     }
                 },
-                Ok(None) => { 
-                    tauri::async_runtime::sleep(std::time::Duration::from_millis(1)).await;
-                    continue;
-                },
+                Ok(None) => continue,
                 Err(e) => {
                     logger::error_with("MM2T Read: error reading bytes {}", e);
-                    let _ = speaker_tx.send(SpeakerNotification::RadioError)
-                        .await;
+                    let _ = speaker_tx.send(SpeakerNotification::RadioError).await;
                     break
                 },
             }
