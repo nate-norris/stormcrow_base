863722f9f4c9ddfd8456480ba510e9899db740eb
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-11 13:09:48 -0700

    implemented strategy pattern for packet handler

--- src-tauri/src/mm2t_read/models.rs
+++ src-tauri/src/mm2t_read/models.rs
@@ -1,4 +1,6 @@
+use tauri::{AppHandle, Emitter};
 use serde::Serialize;
+use utils::mm2t::DecodedPacket;
 
 #[derive(Debug, Serialize)]
 pub(crate) struct WeatherData {
@@ -11,7 +13,7 @@ pub(crate) struct WeatherData {
     pub baro: f32,
 }
 impl WeatherData {
-    pub(crate) fn from_payload(payload: &[u8]) -> anyhow::Result<Self> {
+    fn from_payload(payload: &[u8]) -> anyhow::Result<Self> {
         Ok(Self {
             site_id: payload[0],
             altitude: f32::from_le_bytes(payload[1..5].try_into()?),
@@ -21,5 +23,37 @@ impl WeatherData {
             humidity: f32::from_le_bytes(payload[17..21].try_into()?),
             baro: f32::from_le_bytes(payload[21..25].try_into()?),
         })
+    }
+}
+
+const PACKET_BOOM: u8 = 0x42;
+const PACKET_WEATHER: u8 = 0x57;
+pub(crate) enum PacketKind {
+    Boom,
+    Weather(WeatherData),
+}
+impl PacketKind {
+    pub(crate) fn from_decode(packet: DecodedPacket) -> anyhow::Result<Self> {
+        match packet.packet_type {
+            PACKET_BOOM => Ok(PacketKind::Boom),
+            PACKET_WEATHER => {
+                let data = WeatherData::from_payload(&packet.payload)?;
+                Ok(PacketKind::Weather(data))
+            },
+            _ => anyhow::bail!("Unknown packet type"),
+        }
+    }
+
+    pub(crate) fn handle(self, app: &AppHandle) -> anyhow::Result<()> {
+        match self {
+            PacketKind::Boom => {
+                app.emit("boom", ())?;
+            },
+            PacketKind::Weather(data) => {
+                app.emit("weather", &data)?;
+            }
+        }
+
+        Ok(())
     }
 }
--- src-tauri/src/mm2t_read/packet_handler.rs
+++ src-tauri/src/mm2t_read/packet_handler.rs
@@ -1,33 +1,16 @@
 
-use tauri::{AppHandle, Emitter};
+use tauri::AppHandle;
 use utils::mm2t::DecodedPacket;
 use utils::logger;
-use super::models::WeatherData;
-
-const PACKET_BOOM: u8 = 0x42;
-const PACKET_WEATHER: u8 = 0x57;
+use super::models::PacketKind;
 
 pub(crate) fn handle_packet(packet: DecodedPacket, app_handle: &AppHandle) {
-    match packet.packet_type {
-        PACKET_BOOM => handle_boom(app_handle),
-        PACKET_WEATHER => handle_weather(&packet.payload, app_handle),
-        _ => logger::error("MM2T incorrect packet type identified"),
-    }
-}
-
-fn handle_boom(handle: &AppHandle) {
-    if let Err(e) = handle.emit("boom", ()) {
-        logger::error_with("Boom packet: failed event emit", e);
-    }
-}
-
-fn handle_weather(payload: &[u8], handle: &AppHandle) {
-    match WeatherData::from_payload(payload) {
-        Ok(weather_data) => {
-            if let Err(e) = handle.emit("weather", &weather_data) {
-                logger::error_with("Weather packet: failed emit event", e);
+    match PacketKind::from_decode(packet) {
+        Ok(kind) => {
+            if let Err(e) = kind.handle(app_handle) {
+                logger::error_with("Packet emit failure", e);
             }
-        },
-        Err(e) => logger::error_with("Weather packet: failed ", e),
+        }
+        Err(e) => logger::error_with("Packet decode error", e)
     }
 }
