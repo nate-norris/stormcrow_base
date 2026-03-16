3f2490a854a10ede9defd5f7a769e8a3f4e6292d
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-16 13:06:20 -0700

    initial observers and derived atom added. Not yet implemented.

--- src/atoms/weatherAtoms.tsx
+++ src/atoms/weatherAtoms.tsx
@@ -1,6 +1,28 @@
+import { atom } from 'jotai'
+import type { WeatherObservation } from "@/models";
 
+/// create atom for WeatherObservers which is an array of Observer
+/// They will be mapped by observer id such as A, B, C, etc
+/// the will need to allow for read, write, and update
+///     consider using derived atoms for this
 
-type WeatherObservers = {
-    "A": {}
+// siteId to WeatherObservation
+type WeatherObservers = Record<string, WeatherObservation>;
+export const weatherObserversAtom = atom<WeatherObservers>({});
+
+// update only atom for last WeatherObservation
+export const updateWeatherObserversAtom = atom(
+  null,
+  (get, set, newObservation: WeatherObservation) => {
+    set(weatherObserversAtom, prev => ({
+      ...prev,
+      [newObservation.siteId]: newObservation,
+    }))
+  }
+);
+
+export const sortedWeatherObserversAtom = atom(get) => {
+    // TODO
+    const observations = Object.values(get(weatherObserversAtom))
+    return observations[0]
 }
-// weatherAtom
