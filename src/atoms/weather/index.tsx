99ca772c32b0e7f97fdde48d601930872b0d96d0
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-17 11:46:01 -0700

    initial commit

---
+++ src/atoms/weather/index.tsx
@@ -0,0 +1,7 @@
+/**
+ * Purpose
+ * ------------
+ * Acts as the event stream processor and state machine for weather events 
+ * sent by the React consumer. The internal Jotai atom will be the State
+ * Store / Sink to provide to UI components.
+ */
