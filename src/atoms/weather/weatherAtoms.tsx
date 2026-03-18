217dde4f27b130754b6ce958394d7412f5119cb8
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-18 10:57:33 -0700

    removed derived logic

--- src/atoms/weather/weatherAtoms.tsx
+++ src/atoms/weather/weatherAtoms.tsx
@@ -43,8 +43,3 @@ export const updateWeatherObserversAtom = atom(
   }
 );
 
-export const sortedWeatherObserversAtom = atom(get) => {
-    // TODO
-    const observations = Object.values(get(weatherObserversAtom))
-    return observations[0]
-}
