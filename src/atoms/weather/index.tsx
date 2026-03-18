b52083a5df5115e51c09cd68a78887c27ebe5d9c
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-18 10:56:39 -0700

    added public re-exports

--- src/atoms/weather/index.tsx
+++ src/atoms/weather/index.tsx
@@ -5,3 +5,7 @@
  * sent by the React consumer. The internal Jotai atom will be the State
  * Store / Sink to provide to UI components.
  */
+
+export type { WeatherPacket } from "./models";
+export { WeatherProcessor } from "./processor";
+export { sortedWeatherObserversAtom } from "./derived";
