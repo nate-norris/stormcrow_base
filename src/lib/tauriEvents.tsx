e6e6e1079b002eb05c51c090b4417f8f77a6dd8a
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-18 10:58:50 -0700

    transitioned this to only transport layer in stream process

--- src/lib/tauriEvents.tsx
+++ src/lib/tauriEvents.tsx
@@ -1,6 +1,8 @@
 import { Event, listen } from "@tauri-apps/api/event";
-import type { WeatherPacket, WeatherObservation, WindCalcs } from "@/models";
-import { getWindCalculations } from "./windCalculations";
+// import type { WeatherPacket, WeatherObservation, WindCalcs } from "@/models";
+// import { getWindCalculations } from "./windCalculations";
+// import { WeatherReducer } from "@/atoms"
+import { WeatherProcessor, WeatherPacket } from "@/atoms";
 
 type TauriEvents =  {
     weather: WeatherPacket;
@@ -8,44 +10,8 @@ type TauriEvents =  {
 }
 
 export async function initTauriListeners() {
-    await listen<TauriEvents["weather"]>("weather", handleWeatherObservation);
-
-    listen("weather", (event) => {
-  weatherReducer.dispatch({
-    type: "OBSERVATION_RECEIVED",
-    payload: event.payload
-  });
-});
+    const weatherProcessor = new WeatherProcessor();
+    await listen<TauriEvents["weather"]>("weather", (event) => {
+        weatherProcessor.handlePacket(event.payload)
+    });
 }
-
-function handleWeatherObservation(event: Event<WeatherPacket>) {
-    const raw: WeatherPacket = event.payload;
-    const windTo = (raw.windDir + 180) % 360; // swap wind from to heading
-    const c: WindCalcs = getWindCalculations(0, raw.windFull, windTo);
-
-    // atom for current testing configuration
-    const observation: WeatherObservation = {
-        ...
-    }
-
-    const observation: WeatherObservation = {
-        siteId: raw.siteId,
-        time: new Date(),
-        altitude: raw.altitude,
-        windFull: raw.windFull,
-        windDir: windTo,
-        cross: 0,
-        headTail: 0,
-        temp: raw.temp,
-        humidity: raw.humidity,
-        baro: raw.baro
-    }
-
-    store.set(weatherAtom, (prev) => {{
-        ...prev,
-        [observation.siteId]: observation,
-    }});
-}
-// get weather <data value=""></data>
-//     <WeatherValue></WeatherValue>
-//     push onto weather stack for multiple weather 
