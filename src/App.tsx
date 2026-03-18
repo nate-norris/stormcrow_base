3daa7927811e190258d16b10ad2dfd51158dd08b
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-18 11:11:46 -0700

    added jotai store creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import "./index.css";
+import { Provider } from "jotai";
+import { store } from "@/state";
 // import QEForm from "@/components/forms/QEForm";
 // import DbForm from "@/components/forms/DbForm";
 
 function App() {
   return (
-    <div className="min-h-screen">
-      {/* <QEForm />
-      <DbForm /> */}
-      <p>Hello</p>
-    </div>
+    <Provider store={store}>
+      <div className="min-h-screen">
+        {/* <QEForm />
+        <DbForm /> */}
+        <p>Hello</p>
+      </div>
+    </Provider>
+    
   );
 }
 
