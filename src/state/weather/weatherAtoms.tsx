f7c472e6f414bdc24b3e768ea0954007054ab453
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-24 12:39:03 -0700

    added update type to atom value for state only update

--- src/state/weather/weatherAtoms.tsx
+++ src/state/weather/weatherAtoms.tsx
@@ -1,5 +1,5 @@
 import { atom } from 'jotai'
-import type { WeatherObservation } from "@/models";
+import type { WeatherObservation } from "./models";
 
 /// create atom for WeatherObservers which is an array of Observer
 /// They will be mapped by observer id such as A, B, C, etc
@@ -30,15 +30,21 @@ lifecycle
 
 // siteId to WeatherObservation
 type WeatherObservers = Record<string, WeatherObservation>;
+// type allowing partial update of WeatherObservation
+type  WeatherObservationUpdate = Partial<WeatherObservation> & { siteId: string};
+// primary atom holding WeatherObservers
 export const weatherObserversAtom = atom<WeatherObservers>({});
 
 // update only atom for last WeatherObservation
 export const updateWeatherObserversAtom = atom(
   null,
-  (_get, set, newObservation: WeatherObservation) => {
+  (_get, set, newObservation: WeatherObservationUpdate) => {
     set(weatherObserversAtom, prev => ({
       ...prev,
-      [newObservation.siteId]: newObservation,
+      [newObservation.siteId]: {
+        ...(prev[newObservation.siteId] ?? {}),
+        ...newObservation,
+      } as WeatherObservation
     }))
   }
 );
