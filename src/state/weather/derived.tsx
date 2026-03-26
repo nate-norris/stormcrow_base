5fd1ba903f273817b752cf982d48f7fa4ce0fbfd
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-26 13:10:07 -0700

    updated documentation and added siteIdAtom for iterating WeatherCard

--- src/state/weather/derived.tsx
+++ src/state/weather/derived.tsx
@@ -1,11 +1,24 @@
-import { atom } from 'jotai'
+import { atom, Atom } from 'jotai'
 import { weatherObserversAtom } from "./weatherAtoms";
+import { WeatherObservation } from './models';
 
 /**
+ * Supply individual atom from the provided siteId
+ * 
  * All data is kept current inside weatherObserversAtom and is supplied
  * to decrease re-renders of React components.
  * 
  * @param siteId - identifier for the specific weather site
+ * @returns A Jotai atom containing the WeatherObservation for the site, or undefined if it does not exist
  */
-export const getWeatherSiteAtom = (siteId: string) =>
+export const getWeatherSiteAtom = (siteId: string): Atom<WeatherObservation | undefined> =>
     atom((get) => get(weatherObserversAtom)[siteId]);
+
+/**
+ * Supply all active siteIds tracked.
+ * 
+ * @returns string[] of all siteIds tracked by weatherObserversAtom
+ */
+export const siteIdsAtom = atom((get) => 
+    Object.keys(get(weatherObserversAtom))
+);
