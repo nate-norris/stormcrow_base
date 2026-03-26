cf686ce8d63b113eefa7f280cc5309a41d613649
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-26 12:34:17 -0700

    name changed for public derived atom

--- src/state/weather/index.tsx
+++ src/state/weather/index.tsx
@@ -8,4 +8,4 @@
 
 export type { WeatherPacket } from "./models";
 export { WeatherStreamProcessor } from "./processor";
-// export { sortedWeatherObserversAtom } from "./derived";
+export { getWeatherSiteAtom } from "./derived";
