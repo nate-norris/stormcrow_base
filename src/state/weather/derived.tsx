3dc887558f03cb8fe02cd194cd5651902ddccf3e
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-26 09:39:25 -0700

    temporarily removing sorted atom

--- src/state/weather/derived.tsx
+++ src/state/weather/derived.tsx
@@ -1,7 +1,7 @@
 
 
-export const sortedWeatherObserversAtom = atom(get) => {
-    // TODO
-    const observations = Object.values(get(weatherObserversAtom))
-    return observations[0]
-}
+// export const sortedWeatherObserversAtom = atom(get) => {
+//     // TODO
+//     const observations = Object.values(get(weatherObserversAtom))
+//     return observations[0]
+// }
