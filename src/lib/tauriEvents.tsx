3b78410b46b33b99c29e5308ac2a5f77c3d6ea80
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-04-01 07:52:36 -0700

    Added data transfer object to map to expected domain type for weather

--- src/lib/tauriEvents.tsx
+++ src/lib/tauriEvents.tsx
@@ -2,15 +2,62 @@ import { listen } from "@tauri-apps/api/event";
 import { WeatherPacket } from "@/state";
 import { weatherProcessor } from "./weatherProcessor";
 
+/**
+ * Data transfer object received on TauriEvent:weather.
+ * The site_id on Rust will emit an event where site_id is type u8. This will 
+ * be translated to UI compatible WeatherPacket and combined into 
+ * WeatherObservation.
+ */
+export interface WeatherPayloadDTO {
+  siteId: number;
+  altitude: number;
+  windFull: number;
+  windDir: number;
+  temp: number;
+  humidity: number;
+  baro: number;
+}
+export namespace WeatherPayloadDTO {
+    // convert siteId to string so that u8 becomes char
+    // id will come in as "A"/"B"/etc for each site
+    export function toDomain(dto: WeatherPayloadDTO): WeatherPacket {
+        return {
+            siteId: String.fromCharCode(dto.siteId),
+            altitude: dto.altitude,
+            windFull: dto.windFull,
+            windDir: dto.windDir,
+            temp: dto.temp,
+            humidity: dto.humidity,
+            baro: dto.baro,
+        };
+    }
+}
+
+/**
+ * Type of events that will arrive from Tauri events
+ */
 type TauriEvents =  {
-    weather: WeatherPacket;
+    weather: WeatherPayloadDTO;
     boom: null;
 }
 
+/**
+ * Initialize waiting for Tauri events.
+ * 
+ * Will translate event from list<T>() to proper domain type and pass to the
+ * correct processor.
+ * 
+ * Events take the form:
+ * type Event<T> = {
+ *      event: string;
+ *      payload: T;
+ *      id: number;
+ * }
+ */
 export async function initTauriListeners() {
     await listen<TauriEvents["weather"]>("weather", (event) => {
-        console.log("Tauri event: weather listener");
-        weatherProcessor.handlePacket(event.payload)
+        const packet: WeatherPacket = WeatherPayloadDTO.toDomain(event.payload);
+        weatherProcessor.handlePacket(packet);
     });
 
     await listen<TauriEvents["boom"]>("boom", (_event) => {
