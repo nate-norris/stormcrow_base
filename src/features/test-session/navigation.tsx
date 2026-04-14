e2f3cf87c6583c1c9937aba2d5b60bb58bbb4148
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-04-14 12:40:56 -0700

    initial navigation logic extracted for moving between test mgmt steps

---
+++ src/features/test-session/navigation.tsx
@@ -0,0 +1,20 @@
+import type { StepMode } from "./models";
+import { Step } from "./models";
+
+// options for allowed steps to return to test management menu
+const canGoBackToMenu: StepMode[] = [Step.New, Step.Continue, Step.Delete];
+
+export function createNavigation(
+  step: StepMode,
+  setStep: (s: StepMode) => void
+) {
+  return {
+    go: (next: StepMode) => setStep(next),
+
+    back: () => {
+      if (canGoBackToMenu.includes(step)) {
+        setStep("menu");
+      }
+    },
+  };
+}
