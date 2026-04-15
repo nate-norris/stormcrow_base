fbdf13df4eca733789fe80afb54b71e3f988ad6a
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-04-15 06:03:19 -0700

    temporary layout for test management modal testing

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <Provider store={store}>
-      <div className="min-h-screen">
+      <div className="min-h-screen flex">
         <WeatherSites />
         <TestManagement />
       </div>
