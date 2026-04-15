4c83177642a1bae166c249328b51fb8d633e7aa0
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-04-15 07:37:09 -0700

    passed onSubmit props

--- src/features/test-session/StepContinue.tsx
+++ src/features/test-session/StepContinue.tsx
@@ -1,13 +1,15 @@
 // define the props
 type ContinueProps = {
     onBack: () => void;
+    onSubmit: () => void;
 };
 
-export default function ContinueView({ onBack }: ContinueProps) {
+export default function ContinueView({ onBack, onSubmit }: ContinueProps) {
     return (
         <div>
             <div>Continue</div>
             <button className="btn" onClick={onBack}>Back</button>
+            <button className="btn" onClick={onSubmit}>Submit</button>
         </div>
     );
 }
--- src/features/test-session/StepNew.tsx
+++ src/features/test-session/StepNew.tsx
@@ -1,13 +1,15 @@
 // define the props
 type NewProps = {
     onBack: () => void;
+    onSubmit: () => void;
 };
 
-export default function NewView({ onBack }: NewProps) {
+export default function NewView({ onBack, onSubmit }: NewProps) {
     return (
         <div>
             <div>New</div>
             <button className="btn" onClick={onBack}>Back</button>
+            <button className="btn" onClick={onSubmit}>Submit</button>
         </div>
         
     );
