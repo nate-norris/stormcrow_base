447de9930be3976b793f55a316fed4a8c6d563d9
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-04-22 10:25:22 -0700

    updated return type on initiateTest

--- src/features/test-session/sessionService.ts
+++ src/features/test-session/sessionService.ts
@@ -1,17 +1,18 @@
 import { invoke } from "@tauri-apps/api/core";
-import type { Test } from "@/models";
+import type { Test, WindWarningConfig } from "@/models";
+
+// TODO add error handling to the functions
 
 export async function getLastTest(): Promise<Test | null> {
-  return await invoke("get_last_test_command");
+  return await invoke<Test | null>("get_last_test_command");
 }
 
 export async function getTests(): Promise<Test[]> {
-  return await invoke("get_tests_command");
+  return await invoke<Test[]>("get_tests_command");
 }
 
-export async function initiateTest(name: string): Promise<Test> {
-  const [test] = await invoke<[Test, unknown]>("initiate_test_command", { name });
-  return test;
+export async function initiateTest(name: string): Promise<[Test, WindWarningConfig]> {
+  return await invoke<[Test, WindWarningConfig]>("initiate_test_command", { name });
 }
 
 export async function deleteTest(name: string): Promise<void> {
