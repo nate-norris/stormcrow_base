9df0f87057afdb64b3b3cb4f012478144cd72feb
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-04-24 07:12:06 -0700

    added skipping to continue step if booting app

--- src/features/test-session/TestModal.tsx
+++ src/features/test-session/TestModal.tsx
@@ -3,12 +3,19 @@ import { useState } from "react";
 import { Modal } from "@/components/ui/modal";
 import TestManagement from "./TestManageModal";
 
-export default function TestModal() {
+type Props = {
+    isOnStartup: boolean; // application is booting
+}
+export default function TestModal({ isOnStartup }: Props) {
     const [open, setOpen] = useState(true);
 
     return(
         <Modal isOpen={open} setIsOpen={setOpen}>
-            <TestManagement onComplete={() => setOpen(false)} />
+            <TestManagement 
+                onComplete={() => setOpen(false)} // close modal
+                // skip to continue step on startup if available
+                allowDefaultContinue={isOnStartup}
+            />
         </Modal>
     );
 }
