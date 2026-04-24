fed37d42775f9765531d450e5471afd30d50eaeb
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-04-24 09:14:39 -0700

    added react-icons library

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -1,6 +1,7 @@
 import { createPortal } from "react-dom";
 import FocusLock from "react-focus-lock";
 import { motion, AnimatePresence } from "framer-motion";
+import { FiX } from "react-icons/fi";
 
 type ModalProps = {
   isOpen: boolean;
@@ -25,7 +26,7 @@ export function Modal({ isOpen, setIsOpen=() => {}, children }: ModalProps) {
           onMouseDown={() => setIsOpen(false)}
         >
           <motion.div
-            className="w-full max-w-md rounded-xl bg-white p-6 shadow-xl"
+            className="relative w-full max-w-md rounded-xl bg-white p-6 shadow-xl"
             onMouseDown={(e) => e.stopPropagation()}
             initial={{ opacity: 0, scale: 0.95 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -33,9 +34,9 @@ export function Modal({ isOpen, setIsOpen=() => {}, children }: ModalProps) {
             transition={{ duration: 0.15 }}
           >
             <button
-              className="absolute top-2 right-2"
+              className="absolute top-3 right-3 text-gray-500 hover:text-black transition-all hover:scale-120"
               onClick={() => setIsOpen(false)}>
-              X
+              <FiX size={20} className="stroke-[3.5]" />
             </button>
             {children}
           </motion.div>
