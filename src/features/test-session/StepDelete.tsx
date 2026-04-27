f2b1f1399a2571a21188619a70b5045accb8c3c8
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-04-27 10:39:27 -0700

    state for confirm delete added; cleared selection conflict; layout

--- src/features/test-session/StepDelete.tsx
+++ src/features/test-session/StepDelete.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { ModalBackButton } from "@/components/ui/modal";
+import { Button } from "@/components/ui/button";
 import { Test } from "@/models";
 import { TestSessionSelector } from "./widgets";
 
@@ -10,53 +11,64 @@ type DeleteProps = {
 };
 
 export default function DeleteView({ onBack, tests }: DeleteProps) {
-    // TODO show warning if trying to delete current active test session
-    //      do not allow delete to finalize
+
     const [selectedId, setSelectedId] = useState<number | null>(null);
-    const [isSelectionConflict, setIsSelectionConflict] = useState(false);
+    const [isSelectionConflict, setIsSelectionConflict] = useState<boolean>(false);
+    const [showConfirmDialog, setShowConfirmDialog] = useState<boolean>(false);
 
     const handleSubmit = async () => {
+
         // confirm user is not attempting to delete an active test session
         const fakeId = 2; // TODO pull test selected id from jotai
         if (fakeId === selectedId) {
             setIsSelectionConflict(true);
             return;
+        } else {
+            setIsSelectionConflict(false);
         }
-    };
 
-    const handleCancel = async () => {
-        setSelectedId(null);
-    }
+        setShowConfirmDialog(true);
+    };
 
     return (
 
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col items-center gap-3">
             <ModalBackButton action={onBack} />
-            {!selectedId && (
+            <h2 className="text-xl font-semibold mt-5">Delete a test</h2>
+
+            {/* primary test selection */}
+            {!showConfirmDialog && (
                 <>
-                    <div>Delete</div>
+                    <div className="flex flex-col self-stretch">
                         <TestSessionSelector 
                             tests={tests}
                             selectedId={selectedId}
                             onChange={setSelectedId}
                         />
-                    {isSelectionConflict ? (
-                            <p className="text-red-900">Cannot delete an active test.</p>
-                    ): (<></>)}
-                    <button className="btn" onClick={handleSubmit}>Submit</button>
+                        <p className={`text-sm mt-1 text-red-900 ${isSelectionConflict ? "visible" : "invisible"}`}>
+                            Cannot delete a test currently in session.
+                        </p>
+
+                    </div>
+                    <div className="w-full flex justify-end">
+                        <Button className="w-1/3" onClick={handleSubmit}>
+                            Go
+                        </Button>
+                    </div>
                 </>
             )}
 
-            {!selectedId && (
-                <></>
+            {/* confirmation of test selection for deletion */}
+            {showConfirmDialog && (
+                <ConfirmDeleteView />
             )}
     </div>
     );
 }
 
-
 function ConfirmDeleteView() {
     return (
+        // TODO use shadcn AlertDialog
         <></>
     );
 }
