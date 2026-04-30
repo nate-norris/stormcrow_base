9ccdfcb51ea69286a0ef275b059d896cb37515b8
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-04-30 07:18:20 -0700

    reduced height of nav

--- src/components/layouts/TopNav.tsx
+++ src/components/layouts/TopNav.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function TopNav({ left, center }: { left: ReactNode; center: ReactNode }) {
   return (
-    <div className="sticky top-0 z-50 h-14 w-full border-b bg-white flex items-center">
+    <div className="sticky top-0 z-50 h-12 w-full border-b bg-white flex items-center">
       <div className="flex items-center w-full px-4">
         <div className="flex-shrink-0">{left}</div>
         <div className="absolute left-1/2 -translate-x-1/2">{center}</div>
