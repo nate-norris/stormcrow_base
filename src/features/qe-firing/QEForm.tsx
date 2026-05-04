636eb65d1afc7500489d45949a0326bea751627a
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-05-04 07:15:13 -0700

    QE Form working as primary view for firing

--- src/features/qe-firing/QEForm.tsx
+++ src/features/qe-firing/QEForm.tsx
@@ -1,33 +1,51 @@
 import { useAtom } from "jotai";
-import { lotAtom } from "@/atoms/logQEAtom";
 
-import Dodic from "@/components/widgets/DodicInput";
-import QETypeSelector from "@/components/widgets/QETypeSelector";
-import QECountSpinner from "@/components/widgets/QECountSpinner";
+import { activeTestAtom, dodicAtom, lotAtom, qeCountAtom, qeTypeAtom } from "@/state";
+import { DodicInput } from "./widgets";
+// import { lotAtom } from "@/atoms/logQEAtom";
 
-export default function QEForm() {
+// import Dodic from "@/components/widgets/DodicInput";
+// import QETypeSelector from "@/components/widgets/QETypeSelector";
+// import QECountSpinner from "@/components/widgets/QECountSpinner";
 
-    const [lot, setLot] = useAtom(lotAtom);
+export function QEForm() {
+
+    const [test] = useAtom(activeTestAtom);
 
     function handleSubmit() {
         alert("clicked")
     }
 
     return (
-        <div className="bg-zinc-400">
-            <Dodic />
-            <div>
-                <label htmlFor="lot">LOT</label>
-                <input type="text" id="lot" value={lot}
-                    onChange={(e) => setLot(e.target.value)} />
+        <div className="p-4">
+            <h2 className="text-center text-xl font-semibold mb-4">
+                Current QE
+            </h2>
+            <div className="mb-2">
+                <span className="font-medium">Test: </span>
+                <span>{test ? test.name : ""}</span>
             </div>
-            <QETypeSelector />
-            <QECountSpinner />
-            <label htmlFor="logqe">OVERWRITING</label>
-            <button className="bg-amber-400" id="logqe" onClick={handleSubmit}>
-                Log QE
-            </button>
+            <div className="mb-3">
+                <DodicInput />
+            </div>
         </div>
-        
     );
 }
+
+
+// div className="bg-zinc-400">
+//             <Dodic />
+//             <div>
+//                 <label htmlFor="lot">LOT</label>
+//                 <input type="text" id="lot" value={lot}
+//                     onChange={(e) => setLot(e.target.value)} />
+//             </div>
+//             <QETypeSelector />
+//             <QECountSpinner />
+//             <label htmlFor="logqe">OVERWRITING</label>
+//             <button className="bg-amber-400" id="logqe" onClick={handleSubmit}>
+//                 Log QE
+//             </button>
+//         </div>
+        
+//     );
---
+++ src/features/qe-firing/index.ts
@@ -0,0 +1,1 @@
+export { QEForm } from "./QEForm";
