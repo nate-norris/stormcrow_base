f9ca3336d8e4db8a7b572df5fb7ba4e153a99a6b
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-05-04 06:27:23 -0700

    added TODO to reset atoms upon test change

--- src/state/logQEAtom.tsx
+++ src/state/logQEAtom.tsx
@@ -4,3 +4,15 @@ export const dodicAtom = atom<string>("");
 export const lotAtom = atom<string>("");
 export const qeCountAtom = atom<string>("1");
 export const qeTypeAtom = atom<string>("");
+
+//TODO overwrite DODIC on test change to null if not QEs for this test or last logged QE
+/*
+TODO
+overwrite the above atoms upon activeTestAtomChange
+if there are no QEs logged for the test then empty values for all
+if there is at least one QE then for the last time stamped log
+DODIC = last DODIC
+LOT = last LOT
+qeCount = last count + 1
+qeType = last qeType (TR or WS)
+*/
