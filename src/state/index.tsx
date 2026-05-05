9bc8f683607e831d51596e781e983f013f5f0cf0
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-05-05 10:59:14 -0700

    added export of auto log atom

--- src/state/index.tsx
+++ src/state/index.tsx
@@ -1,5 +1,5 @@
 export * from "./weather";
 export { activeTestAtom } from "./testAtom";
 export { activeConfigAtom } from "./windWarnAtom";
-export { dodicAtom, lotAtom, qeCountAtom, qeTypeAtom } from "./logQEAtom";
+export { dodicAtom, lotAtom, qeCountAtom, qeTypeAtom, autoLogAtom } from "./logQEAtom";
 export * from "./store";
