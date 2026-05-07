e2ce50e612f8a6949d4838eec14090cc3d8e5f97
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-05-07 09:38:14 -0700

    added basic handle click for logQE

--- src/features/qe-firing/log_qe.tsx
+++ src/features/qe-firing/log_qe.tsx
@@ -1,14 +1,22 @@
-import { useAtom } from "jotai";
-
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import logQE from "./actions/index";
 
 // TODO: check overwrite based off testQEsAtom
 export default function LogQE() {
+    const handleClick = async () => {
+        try {
+            // TODO: set loading
+            await logQE();
+        } catch (e) {
+            // TODO: error handle
+        } // TODO add finally
+    }
+
     return (
         <div className="flex flex-col items-center gap-1">
             <Badge variant="destructive">Overwriting</Badge>
-            <Button className="w-30">Log QE</Button>
+            <Button onClick={handleClick} className="w-30">Log QE</Button>
         </div>
     );
 }
