6570d4e0cb064ae9ce6c46c5b8e328a8a3f58a6d
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-05-26 08:58:57 -0700

    mark variable unused

--- src/features/qe-firing/actions/validateQE.ts
+++ src/features/qe-firing/actions/validateQE.ts
@@ -1,6 +1,6 @@
 // TODO: validate test, dodic, lot, qe, qe type selection
 
-export function validateQE(payload: string) {
+export function validateQE(_payload: string) {
 
   // future: overwrite detection, range checks, etc
 }
