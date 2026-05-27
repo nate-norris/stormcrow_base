f0e3489fa32277fb00181dec4f2dbcf0ab0b1cb0
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-05-27 12:29:38 -0700

    no longer swap atom but initiate with mock data; added atom updates fns

--- src/features/incoming-weather/state/weatherObserversAtom.ts
+++ src/features/incoming-weather/state/weatherObserversAtom.ts
@@ -1,12 +1,16 @@
 import { atom, Atom } from 'jotai'
 
-import { WeatherObservation } from '../core/models';
-import { liveWeatherObserversAtom } from './liveWeather';
-import { mockWeatherObserversAtom } from './mockWeather';
+import { WeatherObservation, WeatherUpdate } from '../core/models';
+import { mockData } from "./mockWeather";
 
+// set if Vite .env is set to use mock weather
 const useMock =
   (import.meta.env.VITE_USE_MOCK_WEATHER ?? "false") === "true";
-export const weatherObserversAtom = useMock ? mockWeatherObserversAtom : liveWeatherObserversAtom;
+// pull initial mocked weather if present
+const initialWeather = useMock ? mockData : {};
+// define the primary atom
+type WeatherObservers = Record<string, WeatherObservation>;
+export const weatherObserversAtom = atom<WeatherObservers>(initialWeather);
 
 /**
  * Supply individual atom from the provided siteId
@@ -27,4 +31,52 @@ export const getWeatherSiteAtom = (siteId: string): Atom<WeatherObservation | un
  */
 export const siteIdsAtom = atom((get) => 
     Object.keys(get(weatherObserversAtom))
+);
+
+
+// Update the atom with either a new WeatherObservation or
+// WeatherStatus update.
+export const updateWeatherObserversAtom = atom(
+  null,
+  (_get, set, update: WeatherUpdate) => {
+    set(weatherObserversAtom, prev => {
+      switch (update.type) {
+        // new observation
+        case "observation":
+          return {
+            ...prev,
+            [update.data.siteId]: update.data,
+          };
+
+        // status update 
+        case "status": {
+          const existing = prev[update.siteId];
+          if (!existing) return prev; // no update needed
+
+          return {
+            ...prev,
+            [update.siteId]: {
+              ...existing,
+              status: update.status,
+            },
+          };
+        }
+
+        default:
+          return prev;
+      }
+    });
+  }
+);
+
+// delete WeatherObservation
+export const deleteWeatherObserverAtom = atom(
+  null,
+  (_get, set, siteId: string) => {
+    set(weatherObserversAtom, prev => {
+      const next = { ... prev };
+      delete next[siteId];
+      return next;
+    });
+  }
 );
