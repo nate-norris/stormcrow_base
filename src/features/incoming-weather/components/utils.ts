153192d701b302603426de0816113e0fc6c4a82d
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-05-27 12:30:53 -0700

    added function getStatusColor

--- src/features/incoming-weather/components/utils.ts
+++ src/features/incoming-weather/components/utils.ts
@@ -1,3 +1,6 @@
+import { WeatherStatus, WindState } from "../core/models";
+import type { WeatherS } from "../core/models";
+
 export function formatUnixToTime(ms: number) {
   const date = new Date(ms);
 
@@ -8,4 +11,15 @@ export function formatUnixToTime(ms: number) {
   const seconds = pad(date.getSeconds());
 
   return `${hours}:${minutes}:${seconds}`;
+}
+
+export function getStatusColor(status: WeatherS, state: WindState): string {
+  if (status == WeatherStatus.NotReceiving || state == WindState.Critical)
+    return "bg-red-500";
+  if (status == WeatherStatus.Stale || state == WindState.Warn)
+    return "bg-yellow-500";
+  if (status == WeatherStatus.Receiving || state == WindState.Ok) 
+    return "bg-green-500";
+  
+  return "bg-gray-500";
 }
