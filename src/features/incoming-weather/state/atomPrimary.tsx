27e09b7e6d275221fe23f7723a5f889bbfbcab24
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-05-27 08:38:25 -0700

    pulled out atom specific to wind logs

--- src/features/incoming-weather/state/atomPrimary.tsx
+++ src/features/incoming-weather/state/atomPrimary.tsx
@@ -1,13 +1,9 @@
 import { atom } from 'jotai'
-import type { WeatherObservation, WeatherUpdate, WindFullEvent } from "../core/models";
+import type { WeatherObservation, WeatherUpdate } from "../core/models";
 
 // primary atom of siteId to WeatherObservation
 type WeatherObservers = Record<string, WeatherObservation>;
 export const weatherObserversAtom = atom<WeatherObservers>({});
-
-// secondary log atom of siteId to timestamped wind value
-type WindLog = Record<string, WindFullEvent>;
-export const windLogAtom = atom<WindLog>({});
 
 // Update the atom with either a new WeatherObservation or
 // WeatherStatus update.
@@ -50,41 +46,6 @@ export const deleteWeatherObserverAtom = atom(
   (_get, set, siteId: string) => {
     set(weatherObserversAtom, prev => {
       const next = { ... prev };
-      delete next[siteId];
-      return next;
-    });
-  }
-);
-
-// three minute log of full wind value
-export const updateWindLogAtom = atom(
-  null,
-  (_get, set, obs: WeatherObservation) => {
-    set(windLogAtom, prev => {
-      const newEntry: WindFullEvent = { time: obs.time, windFull: obs.windFull };
-
-      const updated: WindLog = {
-        ...prev,
-        [obs.siteId]: newEntry,
-      };
-
-      // Drop old entries beyond the timeframe
-      const cutoff = obs.time - 3 * 60_000; // 3 minutes
-      Object.keys(updated).forEach(siteId => {
-        if (updated[siteId].time < cutoff) delete updated[siteId];
-      });
-
-      return updated;
-    });
-  }
-);
-
-// delete wind value tracking
-export const deleteWindLogAtom = atom(
-  null,
-  (_get, set, siteId: string) => {
-    set(windLogAtom, prev => {
-      const next = { ...prev };
       delete next[siteId];
       return next;
     });
