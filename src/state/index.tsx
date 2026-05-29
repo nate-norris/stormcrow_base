252c3d92f44484aec4f2c67a6790b909a2893bd2
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-05-29 07:30:40 -0700

    removed global state exports

--- src/state/index.tsx
+++ src/state/index.tsx
@@ -1,4 +1,2 @@
-export { activeTestAtom } from "./testAtom";
-export { activeConfigAtom } from "./windWarnAtom";
 export { dodicAtom, lotAtom, qeCountAtom, qeTypeAtom, autoLogAtom } from "./logQEAtom";
 export * from "./store";
