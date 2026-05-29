4839a1a88bd601f7bbb97b7a232208670de75c88
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-05-29 08:02:13 -0700

    updated layout; added TODO for toast success when no test selected

--- src/features/wind-warnings/components/SettingsForm.tsx
+++ src/features/wind-warnings/components/SettingsForm.tsx
@@ -21,68 +21,82 @@ export default function WindWarningForm() {
       setActiveWindConfig(draftConfig);
       toast.success("Configuration successfully updated.")
     } catch (err) {
+      // TODO confirm why empty test is allowing updateConfiguration
+      // may just need conditional check here.... db is probably checking for update where test id is -1
       toast.error("Configuration failed to update.");
     }
   }
 
   return (
-    <div className="max-w-md mx-auto p-6 rounded-2xl shadow-lg border">
+    <div className="max-w-md mx-auto p-6 rounded-2xl shadow-lg border text-sm font-medium items-center space-y-4">
       <h2 className="text-xl font-semibold mb-4">Wind Configuration</h2>
 
       {/* Max Wind Speed */}
-      <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">Max Wind Speed (mph)</label>
-        <input
-          type="number"
-          step="1"
-          value={draftConfig.maxWind}
-          onChange={(e) =>
-            setDraftConfig(prev => ({
-              ...prev,
-              maxWind: Number(e.target.value)
-            }))
-          }
-          className="w-full p-2 border rounded-lg"
-        />
+      <div className="flex items-center gap-4">
+        <label>Max Wind Speed</label>
+        <span>
+          <input
+            type="number"
+            step="1"
+            value={draftConfig.maxWind}
+            onChange={(e) =>
+              setDraftConfig(prev => ({
+                ...prev,
+                maxWind: Number(e.target.value)
+              }))
+            }
+            className="w-25 p-2 border rounded-lg"
+          />
+          <label> mph</label>
+        </span>
+        
       </div>
 
       {/* Threshold Percent */}
-      <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">Threshold (%)</label>
-        <input
-          type="number"
-          // placeholder="1 - 100"
-          value={draftConfig.thresholdPercent}
-          onChange={(e) =>
-            setDraftConfig(prev => ({
-              ...prev,
-              thresholdPercent: Number(e.target.value)
-            }))
-          }
-          className="w-full p-2 border rounded-lg"
-        />
+      <div className="flex items-center gap-4">
+        <label>Threshold</label>
+        <span>
+          <input
+            type="number"
+            // placeholder="1 - 100"
+            value={draftConfig.thresholdPercent}
+            onChange={(e) =>
+              setDraftConfig(prev => ({
+                ...prev,
+                thresholdPercent: Number(e.target.value)
+              }))
+            }
+            className="w-25 p-2 border rounded-lg"
+          />
+          <label className="font-bold"> %</label>
+
+        </span>
+        
       </div>
 
       {/* Weapon Orientation */}
-      <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">Weapon Degrees (True North)</label>
-        <input
-          type="number"
-          placeholder="0 - 360"
-          value={draftConfig.gunOrient}
-          onChange={(e) =>
-            setDraftConfig(prev => ({
-              ...prev,
-              gunOrient: Number(e.target.value)
-            }))
-          }
-          className="w-full p-2 border rounded-lg"
-        />
+      <div className="flex items-center gap-4">
+        <label>Weapon</label>
+        <span>
+          <input
+            type="number"
+            placeholder="0 - 360"
+            value={draftConfig.gunOrient}
+            onChange={(e) =>
+              setDraftConfig(prev => ({
+                ...prev,
+                gunOrient: Number(e.target.value)
+              }))
+            }
+            className="w-25 p-2 border rounded-lg"
+          />
+          <label> ° N True</label>
+        </span>
       </div>
 
       {/* Expected Sites */}
-      <div className="mb-6">
-        <label className="block text-sm font-medium mb-1">Expected Weather Sites</label>
+      <div className="mb-6 flex items-center gap-4">
+        <label>Expected Sites</label>
         <input
           type="number"
           step="1"
@@ -93,7 +107,7 @@ export default function WindWarningForm() {
               expectedSites: Number(e.target.value)
             }))
           }
-          className="w-full p-2 border rounded-lg"
+          className="w-25 p-2 border rounded-lg"
         />
       </div>
 
