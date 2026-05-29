c9bbc76b53f3aa81a4fe9aaf931c58cb2eb69c29
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-05-29 07:22:31 -0700

    merged with other types

--- src/features/test-session/models.tsx
+++
@@ -1,9 +0,0 @@
-// modes for test management selections
-export const Step = {
-  Menu: "menu",
-  New: "new",
-  Continue: "continue",
-  Delete: "delete",
-} as const;
-
-export type StepMode = typeof Step[keyof typeof Step];
