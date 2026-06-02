d054492d447545f9a57598ee27dee04a23aa78c1
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-02 10:08:52 -0700

    used shadcn Accordion for side panel weather sites/wind config

--- src/components/layouts/FiringView.tsx
+++ src/components/layouts/FiringView.tsx
@@ -1,9 +1,36 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 import { WeatherSites } from "@/features/incoming-weather";
+import { WindWarningForm } from "@/features/wind-warnings";
 
 export function FiringView() {
   return (
-    <div className="flex h-full min-h-0">
-        <WeatherSites />
+    <div className="flex h-full">
+      {/* LEFT SIDE PANEL */}
+      <div className="w-1/5 max-w-96 min-w-64 p-2">
+        <Accordion type="multiple" defaultValue={["sites"]}>
+          <AccordionItem value="sites">
+            <AccordionTrigger>Weather Sites</AccordionTrigger>
+            <AccordionContent><WeatherSites /></AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="config">
+            <AccordionTrigger>Wind Configuration</AccordionTrigger>
+            <AccordionContent><WindWarningForm /></AccordionContent>
+          </AccordionItem>
+
+        </Accordion>
+      </div>
+
+      {/* RIGHT SIDE */}
+      <div className="flex-1 bg-gray-500">
+        {/* Main content here */}
+      </div>
     </div>
   );
 }
+
+// min-h-0, h-full, h-screen
