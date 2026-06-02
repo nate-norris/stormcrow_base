664d766fa31efc4f002b9dc36122d2ee9e41e693
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-02 09:39:09 -0700

    simplified App component. Moved layout to coordinator

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,22 @@
 import "./index.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Provider } from "jotai";
 import { Toaster } from "@/components/ui/sonner";
 
 import { store } from "@/state";
 import { bootstrapApp } from "./bootstrap";
-import { TestModal } from "./features/test-session";
-import { TopNav } from "@/components/layouts/TopNav";
-import { FiringView } from "@/components/layouts/FiringView";
-import { QETableView } from "@/components/layouts/QETableView";
-import { AppTabs, AppMenu } from "@/features/navigation";
+import { AppCoordinator } from "@/app-shell";
 
 function App() {
-  // Test session configuration including adding, deleting or continuing
-  //    previous test sessions.
-  //
-  // TestModal can be revealed by:
-  // 1. On booting the test session should be selected
-  // 2. When the user selects the option from the app menu
-  //
-  // TestModal can be hidden by:
-  // 1: Selecting outside of the modal
-  // 2: Selecting the exit button within the modal
-  // 3: Finilizing actions within the modal (New/Continue)
-  const [isTestManagementOpen, setIsTestManagementOpen] = useState<boolean>(true);
-  const [isBooting, setIsBooting] = useState<boolean>(true);
-  const [view, setView] = useState("firing");
 
   useEffect(() => {
     bootstrapApp();
   }, []);
 
-  const handleTestManagementClose = () => {
-    setIsTestManagementOpen(false);
-    setIsBooting(false); // will always be false after boot
-  }
-
   return (
     <Provider store={store}>
-      <div className="flex h-screen w-full flex-col">
-        <TopNav 
-          left={<AppMenu onOpenTestManagement={() => setIsTestManagementOpen(true)}/>}
-          center={<AppTabs view={view} onChange={setView} />}
-        />
-
-        <main className="flex-1 min-h-0">
-          {view === "firing" && <FiringView />}
-          {view === "qes" && <QETableView />}
-        </main>
-        
-        {/* Allow modified step upon app startup */}
-        <TestModal
-          isOpen={isTestManagementOpen}
-          entryMode={isBooting ? "continue-if-possible" : "menu"}
-          onClose={handleTestManagementClose}
-        />
-        <Toaster />
-      </div>
+      <AppCoordinator />
+      <Toaster />
     </Provider>
   );
 }
