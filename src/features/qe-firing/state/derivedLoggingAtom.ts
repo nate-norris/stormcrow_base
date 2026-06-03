69b8d6a0c1fdb707c2393bb8c83a3931004ca0e9
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-03 09:57:22 -0700

    added derived atoms for specific form inputs

---
+++ src/features/qe-firing/state/derivedLoggingAtom.ts
@@ -0,0 +1,43 @@
+import { atom } from "jotai";
+import { QEType } from "../core/models";
+import { activeQEFormAtom } from "./loggingAtom";
+
+export const dodicAtom = atom(
+  get => get(activeQEFormAtom).dodic,
+  (get, set, value: string) => {
+    set(activeQEFormAtom, {
+      ...get(activeQEFormAtom),
+      dodic: value,
+    });
+  }
+);
+
+export const lotAtom = atom(
+  get => get(activeQEFormAtom).lot,
+  (get, set, value: string) => {
+    set(activeQEFormAtom, {
+      ...get(activeQEFormAtom),
+      lot: value,
+    });
+  }
+);
+
+export const qeCountAtom = atom(
+  get => get(activeQEFormAtom).qeCount,
+  (get, set, value: number) => {
+    set(activeQEFormAtom, {
+      ...get(activeQEFormAtom),
+      qeCount: value,
+    });
+  }
+);
+
+export const qeTypeAtom = atom(
+  get => get(activeQEFormAtom).qeType,
+  (get, set, value: QEType) => {
+    set(activeQEFormAtom, {
+      ...get(activeQEFormAtom),
+      qeType: value,
+    });
+  }
+);
