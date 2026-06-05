d38ffaa0a707c83933e40bd5021c9898fff1539b
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-05 08:13:44 -0700

    minimal validation

--- src/features/qe-logging/actions/validateQE.ts
+++ src/features/qe-logging/actions/validateQE.ts
@@ -1,6 +1,12 @@
 // TODO: validate test, dodic, lot, qe, qe type selection
+import { QEInputs } from "../core/qe_inputs";
+import { isCompleteDODICInput } from "../components/DodicInput";
+import { isCompleteLotInput } from "../components/Lot/lotParser";
 
-export function validateQE(_payload: string) {
-
-  // future: overwrite detection, range checks, etc
+export function canLogQE(inputs: QEInputs): boolean {
+  return !!inputs.test && // valid test 
+    isCompleteDODICInput(inputs.qeForm.dodic) && // verified dodic
+    isCompleteLotInput(inputs.qeForm.lot) && // verified lot
+    inputs.qeForm.qeCount >= 1 && // valid QE
+    Object.keys(inputs.observers).length > 0 // at least one site received weather
 }
