6581b898071e5359a3e7a6de06e62be3f160bcec
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-08 15:41:31 -0700

    working: called update to qe count

--- src/features/qe-logging/actions/updateQEState.ts
+++ src/features/qe-logging/actions/updateQEState.ts
@@ -1,1 +1,18 @@
-// TODO: update all affected state
+import { store } from "@/state";
+
+import { QEFormState } from "../core/qe-form";
+import { WeatherRow } from "../core/qe_outputs";
+import { activeQEFormAtom } from "../state/loggingAtom";
+
+export function updateStateUponLog(qeForm: QEFormState, row: WeatherRow) {
+  // increment to next QE in form
+  prepareNextQE(qeForm);
+  // update weather row atom
+}
+
+function prepareNextQE(qeForm: QEFormState) {
+  store.set(activeQEFormAtom, {
+    ...qeForm, // previous values
+    qeCount: qeForm.qeCount + 1,
+  });
+}
