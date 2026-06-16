443ebd8cb6e6d1a14bd5156c628486445fa79e53
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-16 10:40:49 -0700

    fixed: was returning WeatherRow vice WeatherRow[]

--- src/features/qe-logging/actions/persistQE.ts
+++ src/features/qe-logging/actions/persistQE.ts
@@ -3,6 +3,6 @@ import { invoke } from "@tauri-apps/api/core";
 import { QEEntry } from "../core/qe-log";
 import { WeatherRow } from "../core/qe_outputs";
 
-export async function dbPersistQEEntry(entry: QEEntry): Promise<WeatherRow> {
-  return await invoke("insert_new_qe_command", { newQe: entry });
+export async function dbPersistQEEntry(entry: QEEntry): Promise<WeatherRow[]> {
+  return await invoke<WeatherRow[]>("insert_new_qe_command", { newQe: entry });
 }
