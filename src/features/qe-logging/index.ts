f560f87392ac4f2f1acaa183ea5541616ccce685
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-16 10:40:05 -0700

    export types needed in qeEntriesAtom

--- src/features/qe-logging/index.ts
+++ src/features/qe-logging/index.ts
@@ -2,3 +2,5 @@ export { QEForm } from "./components/QEForm";
 export { activeQEFormAtom } from "./state/loggingAtom";
 export { updateQEFormFromLast } from "./services/updateForm";
 export { resetQEForm } from "./services/resetForm";
+export { type WeatherRow } from "./core/qe_outputs";
+export { type QEType } from "./core/qe-types";
