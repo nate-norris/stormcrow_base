c504bd90fca3329bde3652057d510c632affee2a
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-16 10:44:29 -0700

    passed WeatherRow[] to updateStateUponLog; called qe-table/replaceQE()

--- src/features/qe-logging/actions/updateQEState.ts
+++ src/features/qe-logging/actions/updateQEState.ts
@@ -3,11 +3,13 @@ import { store } from "@/state";
 import { QEFormState } from "../core/qe-form";
 import { WeatherRow } from "../core/qe_outputs";
 import { activeQEFormAtom } from "../state/loggingAtom";
+import { replaceQE } from "@/features/qe-table";
 
-export function updateStateUponLog(qeForm: QEFormState, row: WeatherRow) {
+export function updateStateUponLog(qeForm: QEFormState, rows: WeatherRow[]) {
   // increment to next QE in form
   prepareNextQE(qeForm);
   // update weather row atom
+  replaceQE(rows);
 }
 
 function prepareNextQE(qeForm: QEFormState) {
