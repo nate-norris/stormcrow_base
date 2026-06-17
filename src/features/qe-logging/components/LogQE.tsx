d62527bf10148f9612934d111506b20a71dc2b99
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-17 06:05:24 -0700

    useMemo check added if overwriting same QE

--- src/features/qe-logging/components/LogQE.tsx
+++ src/features/qe-logging/components/LogQE.tsx
@@ -1,9 +1,28 @@
+import { useMemo } from "react";
+import { useAtomValue } from "jotai";
+
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import logQE from "../actions/logQE";
+import { type QEKey } from "@/features/qe-table";
+
+import { qeCountAtom, qeTypeAtom } from "../state/derivedLoggingAtom";
+import { qeEntriesAtom } from "@/features/qe-table/state/qeEntries";
 
 // TODO: check overwrite based off testQEsAtom
 export default function LogQE() {
+    const qeCount = useAtomValue(qeCountAtom);
+    const qeType = useAtomValue(qeTypeAtom);
+    const qeEntries = useAtomValue(qeEntriesAtom);
+
+    const qeExists = useMemo(() => {
+        return qeEntries.some(
+            row =>
+                row.count === qeCount &&
+                row.qeType === qeType
+        );
+    }, [qeCount, qeType, qeEntries]);
+
     const handleClick = async () => {
         try {
             // TODO: set loading
@@ -16,7 +35,9 @@ export default function LogQE() {
     return (
         <div className="flex flex-col gap-1">
             <Button onClick={handleClick} className="bg-blue-600">Log QE</Button>
-            <Badge variant="destructive">Overwriting</Badge>
+            {qeExists && (
+                <Badge variant="destructive">Overwriting</Badge>
+            )}
         </div>
     );
 }
