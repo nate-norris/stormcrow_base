9a9a4b611ed41e8eed34a7a947f96588dd0ea06e
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-17 07:35:47 -0700

    atom name change

--- src/features/qe-table/state/weatherRowsAtom.ts
+++ src/features/qe-table/state/weatherRowsAtom.ts
@@ -2,4 +2,4 @@ import { atom } from "jotai";
 
 import type { WeatherRow } from "../core/weatherRow";
 
-export const qeEntriesAtom = atom<WeatherRow[]>([]);
+export const weatherRowsAtom = atom<WeatherRow[]>([]);
