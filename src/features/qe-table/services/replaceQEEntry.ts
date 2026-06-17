a3f615201b7e73d24d4a5fe791eb96fafa0bbfdc
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-17 06:03:23 -0700

    added verification that passed WeatherRow[] are apart of same QE

--- src/features/qe-table/services/replaceQEEntry.ts
+++ src/features/qe-table/services/replaceQEEntry.ts
@@ -6,7 +6,11 @@ import { qeEntriesAtom } from "../state/qeEntries";
 import { removeQERows } from "../actions/removeQERows";
 
 export function replaceQE(rows: WeatherRow[]) {
+    // no qe to delete
     if (rows.length === 0) return;
+    // rows provided are not apart of the same QE
+    if (!isSingleQE(rows)) 
+        throw new Error("Test selection required to log QEs.");
     
     const key: QEKey = {
         count: rows[0].count,
@@ -18,4 +22,14 @@ export function replaceQE(rows: WeatherRow[]) {
         ...removeQERows(allRows, key),
         ...rows,
     ]);
+}
+
+function isSingleQE(rows: WeatherRow[]): boolean {
+  const { count, qeType } = rows[0];
+
+  return rows.every(
+    row =>
+      row.count === count &&
+      row.qeType === qeType
+  );
 }
