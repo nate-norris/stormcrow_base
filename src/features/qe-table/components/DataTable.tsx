d8beae85a782cbf38f712a42fa56a8b07818ebe0
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-22 06:01:21 -0700

    sorting table

---
+++ src/features/qe-table/components/DataTable.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react"
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable,
+  SortingState, getSortedRowModel } from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[],
+  data: TData[],
+}
+
+export function DataTable<TData, TValue>({columns, data,}: 
+  DataTableProps<TData, TValue>) {
+
+  const [sorting, setSorting] = useState<SortingState>([]);
+    
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+  })
+
+  return (
+    <div className="overflow-hidden rounded-md border">
+      <Table>
+        <TableHeader className="bg-slate-600">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id} className="hover:bg-slate-600">
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id} className="text-white">
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-20 text-center font-semibold text-lg">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
---
+++ src/features/qe-table/components/QETable.tsx
@@ -0,0 +1,15 @@
+import { useAtomValue } from "jotai";
+
+import { qeTableRowsAtom } from "../state/tableRowsAtom";
+import { DataTable } from "./DataTable";
+import { columns } from "../core/columns";
+
+export default function QETable() {
+    const rows = useAtomValue(qeTableRowsAtom);
+    return (
+        <div className="container mx-auto py-10">
+            <DataTable columns={columns} data={rows} />
+        </div>
+
+    );
+}
