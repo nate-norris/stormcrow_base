8d072700730e2b0a62427b91db56749ca21930b0
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-22 08:22:03 -0700

    added AlertDeleteDialog to component along with state

--- src/features/qe-table/components/QETable.tsx
+++ src/features/qe-table/components/QETable.tsx
@@ -1,15 +1,44 @@
 import { useAtomValue } from "jotai";
+import { useState } from "react";
 
 import { qeTableRowsAtom } from "../state/tableRowsAtom";
 import { DataTable } from "./DataTable";
-import { columns } from "./columns";
+import { createColumns } from "./columns";
+import type { QEKey } from "../core/qeKey";
+import { AlertDeleteDialog } from "./AlertDelete";
+import { removeQE } from "../services/removeQEEntry";
 
 export default function QETable() {
     const rows = useAtomValue(qeTableRowsAtom);
+    const [deleteQE, setDeleteKey] = useState<QEKey | null>(null);
+    const [reassignQE, setReassignKey] = useState<QEKey | null>(null);
+
+    function onDeleteRequest(key: QEKey) {
+        setDeleteKey(key);
+    }
+
+    function cancelDeleteRequest() {
+        setDeleteKey(null);
+    }
+
+    function onReassignRequest(key: QEKey) {
+        setReassignKey(key);
+    }
+
     return (
-        <div className="container mx-auto py-10">
-            <DataTable columns={columns} data={rows} />
-        </div>
+        <>
+            <div className="container mx-auto py-10">
+                <DataTable columns={createColumns(onDeleteRequest)} data={rows} />
+            </div>
+
+            <AlertDeleteDialog 
+                qeKey={deleteQE}
+                onCancel={cancelDeleteRequest}
+                onConfirm={removeQE}
+            
+            />
+        </>
+        
 
     );
 }
