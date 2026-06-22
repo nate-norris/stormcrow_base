3559cd717d41bd7475b019639acdcaa52bd63f02
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-22 08:22:35 -0700

    confim active test; remove QEBase from database

--- src/features/qe-table/services/removeQEEntry.ts
+++ src/features/qe-table/services/removeQEEntry.ts
@@ -1,10 +1,25 @@
 import { store } from "@/state/store"
 
+import { QEBase } from "@/features/qe-logging";
+import { activeTestAtom } from "@/features/test-session";
 import { type QEKey } from "../core/qeKey";
 import { weatherRowsAtom } from "../state/weatherRowsAtom";
 import { removeQERows } from "../actions/removeQERows";
+import { removeQEDatabase } from "../actions/removeQEDatabase";
 
-export function removeQE(key: QEKey) {
+export async function removeQE(key: QEKey) {
+  const test = store.get(activeTestAtom);
+  if (!test) return;
+
+  // remove from database
+  const base: QEBase = {
+    count: key.count,
+    qeType: key.qeType,
+    testId: test?.id,
+  }
+  await removeQEDatabase(base);
+
+  // update UI table
   store.set(weatherRowsAtom,
     removeQERows(
       store.get(weatherRowsAtom),
