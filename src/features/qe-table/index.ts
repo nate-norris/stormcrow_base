df08d7fdb3b9c0d16d65ccc9356aa035270e2647
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-22 06:01:47 -0700

    removeQE no longer public; added QETable export

--- src/features/qe-table/index.ts
+++ src/features/qe-table/index.ts
@@ -1,7 +1,7 @@
-export { removeQE } from "./services/removeQEEntry";
 export { replaceQE } from "./services/replaceQEEntry";
 export { hydrateQEs } from "./services/hydrateQEs";
 export { type QEKey } from "./core/qeKey";
 export { type WeatherRow } from "./core/weatherRow";
 export { weatherRowsAtom } from "./state/weatherRowsAtom";
 export { lastWeatherRowAtom } from "./state/lastRowAtom";
+export { default as QETable } from "./components/QETable";
