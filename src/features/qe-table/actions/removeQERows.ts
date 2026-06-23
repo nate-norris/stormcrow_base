2ce9aff125f606a39cc0ba4bdf4d8ddef1786efa
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-23 11:01:25 -0700

    moved all reassignment logic out

--- src/features/qe-table/actions/removeQERows.ts
+++ src/features/qe-table/actions/removeQERows.ts
@@ -1,30 +1,13 @@
 import { store } from "@/state/store"
 import { weatherRowsAtom } from "../state/weatherRowsAtom";
-import type { WeatherRow } from "../core/weatherRow";
 import type { QEKey } from "../core/qeKey";
+import { rowsNotSpecifiedByKey } from "./rowsNotByKey";
 
 export function removeQERowsByKey(key: QEKey) {
   store.set(weatherRowsAtom,
       rowsNotSpecifiedByKey(
         store.get(weatherRowsAtom),
         key
-      )
-  );
-}
-
-export function replaceQERowsWithNewRows(newRows: WeatherRow[], key: QEKey) {
-  store.set(weatherRowsAtom, [
-    ...rowsNotSpecifiedByKey(store.get(weatherRowsAtom), key),
-    ...newRows,
-  ]);
-}
-
-function rowsNotSpecifiedByKey(rows: WeatherRow[], key: QEKey): WeatherRow[] {
-  return rows.filter(
-    row =>
-      !(
-        row.count === key.count &&
-        row.qeType === key.qeType
       )
   );
 }
