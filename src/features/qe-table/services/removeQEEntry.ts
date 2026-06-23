0b1d19e344180d8d44f66051f1a6c5deb6d841e4
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-23 11:03:07 -0700

    pulled out direct store updates

--- src/features/qe-table/services/removeQEEntry.ts
+++ src/features/qe-table/services/removeQEEntry.ts
@@ -1,7 +1,5 @@
-import { store } from "@/state/store"
 import { type QEKey } from "../core/qeKey";
-import { weatherRowsAtom } from "../state/weatherRowsAtom";
-import { removeQERows } from "../actions/removeQERows";
+import { removeQERowsByKey } from "../actions/removeQERows";
 import { removeQEDatabase } from "../actions/removeQEDatabase";
 import { buildQEBaseFromKey } from "../actions/buildQEBaseFromKey";
 
@@ -9,13 +7,9 @@ export async function removeQE(key: QEKey) {
   const base = buildQEBaseFromKey(key);
   if (!base) return;
 
+  // remove from database
   await removeQEDatabase(base);
 
   // update UI table
-  store.set(weatherRowsAtom,
-    removeQERows(
-      store.get(weatherRowsAtom),
-      key
-    )
-  );
+  removeQERowsByKey(key);
 }
