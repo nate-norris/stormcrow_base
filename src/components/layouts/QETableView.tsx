155506a247069c20ae878d822fb62b9e7d67b4e4
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-23 12:07:21 -0700

    added and aligned ExportDataButton

--- src/components/layouts/QETableView.tsx
+++ src/components/layouts/QETableView.tsx
@@ -1,8 +1,14 @@
 import { QETable } from "@/features/qe-table";
+import { ExportDataButton } from "@/features/qe-export";
 
 export function QETableView() {
   return (
     <div className="h-full min-h-0 overflow-auto">
+      <div className="container mx-auto">
+        <div className="ml-auto w-32">
+          <ExportDataButton />
+        </div>
+      </div>
       <QETable />
     </div>
   );
