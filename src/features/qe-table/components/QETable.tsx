b899920a1f198882a97b4fa6695507562a26e5ab
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-23 11:03:28 -0700

    display reassign or delete modal based off state

--- src/features/qe-table/components/QETable.tsx
+++ src/features/qe-table/components/QETable.tsx
@@ -6,12 +6,14 @@ import { DataTable } from "./DataTable";
 import { createColumns } from "./columns";
 import type { QEKey } from "../core/qeKey";
 import { AlertDeleteDialog } from "./AlertDelete";
+import { AlertReassignDialog } from "./AlertReassign"
 import { removeQE } from "../services/removeQEEntry";
+import { reassignQE } from "../services/reassignQE";
 
 export default function QETable() {
     const rows = useAtomValue(qeTableRowsAtom);
     const [deleteQE, setDeleteKey] = useState<QEKey | null>(null);
-    const [reassignQE, setReassignKey] = useState<QEKey | null>(null);
+    const [reassign, setReassignKey] = useState<QEKey | null>(null);
 
     function onDeleteRequest(key: QEKey) {
         setDeleteKey(key);
@@ -25,20 +27,30 @@ export default function QETable() {
         setReassignKey(key);
     }
 
+    function cancelReassignRequest() {
+        setReassignKey(null);
+    }
+
     return (
         <>
             <div className="container mx-auto py-10">
-                <DataTable columns={createColumns(onDeleteRequest)} data={rows} />
+                <DataTable columns={createColumns(onDeleteRequest, onReassignRequest)} data={rows} />
             </div>
 
-            <AlertDeleteDialog 
-                qeKey={deleteQE}
-                onCancel={cancelDeleteRequest}
-                onConfirm={removeQE}
-            
-            />
+            {deleteQE && (
+                <AlertDeleteDialog 
+                    qeKey={deleteQE}
+                    onCancel={cancelDeleteRequest}
+                    onConfirm={removeQE}
+                />
+            )}
+            {reassign && (
+                <AlertReassignDialog
+                    qeKey={reassign}
+                    onCancel={cancelReassignRequest}
+                    onConfirm={reassignQE}
+                />
+            )}
         </>
-        
-
     );
 }
