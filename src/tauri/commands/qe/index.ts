7343ad9397b2c07b5dc1c5432a8839f86b115ab7
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-29 11:41:04 -0700

    export qe commands

---
+++ src/tauri/commands/qe/index.ts
@@ -0,0 +1,4 @@
+export { dbPersistQEEntry } from "./persistQE";
+export { reassignQEDatabase } from "./reassignQEDatabase";
+export { removeQEDatabase } from "./removeQEDatabase";
+export { saveTestQEsToPath } from "./saveTestQEsToPath";
