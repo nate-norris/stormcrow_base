b2dcf08d267654465e0d80b4bd1a5c3bb6285a54
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-29 14:26:38 -0700

    defined weatherHandler const and implemented register function

---
+++ src/tauri/events/weather.ts
@@ -0,0 +1,44 @@
+import { listen } from "@tauri-apps/api/event";
+
+import { type WeatherPacket, weatherProcessor } from "@/features/incoming-weather";
+import { TauriEventHandler } from "./models";
+
+/**
+ * Data transfer object received on TauriEvent:weather.
+ * The site_id on Rust will emit an event where site_id is type u8. This will 
+ * be translated to UI compatible WeatherPacket and combined into 
+ * WeatherObservation.
+ */
+interface WeatherEventDTO {
+  siteId: number;
+  altitude: number;
+  windFull: number;
+  windDir: number;
+  temp: number;
+  humidity: number;
+  baro: number;
+}
+namespace WeatherEventDTO {
+    // convert siteId to string so that u8 becomes char
+    // id will come in as "A"/"B"/etc for each site
+    export function toDomain(dto: WeatherEventDTO): WeatherPacket {
+        return {
+            siteId: String.fromCharCode(dto.siteId),
+            altitude: dto.altitude,
+            windFull: dto.windFull,
+            windDir: dto.windDir,
+            temp: dto.temp,
+            humidity: dto.humidity,
+            baro: dto.baro,
+        };
+    }
+}
+
+export const weatherHandler: TauriEventHandler = {
+    async register() {
+        await listen<WeatherEventDTO>("weather", (event) => {
+            const packet: WeatherPacket = WeatherEventDTO.toDomain(event.payload);
+            weatherProcessor.handlePacket(packet);
+        });
+    }
+}
