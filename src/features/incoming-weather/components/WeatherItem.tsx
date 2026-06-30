081b418eee1f9bb96fa51c05ca8c8f69027805c5
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-30 07:13:16 -0700

    added colored icons for wind and receiving state

--- src/features/incoming-weather/components/WeatherItem.tsx
+++ src/features/incoming-weather/components/WeatherItem.tsx
@@ -1,4 +1,5 @@
 import { useAtomValue } from "jotai";
+import { RadioTowerIcon, CloudRainWindIcon } from "lucide-react";
 
 import {
   HoverCard,
@@ -6,8 +7,8 @@ import {
   HoverCardTrigger,
 } from "@/components/ui/hover-card"
 import { weatherSiteAtomFamily } from "../state/weatherSiteAtomFamily";
-import { formatUnixToTime, formatReceivingStatusText, getStatusColor } 
-    from "./utils";
+import { formatUnixToTime, formatReceivingStatusText, 
+    getReceivingStatusColor, getWindStateColor } from "./utils";
 
 export default function WeatherItem({ siteId }: { siteId: string }) {
     const siteWeather = useAtomValue(weatherSiteAtomFamily(siteId));
@@ -18,30 +19,41 @@ export default function WeatherItem({ siteId }: { siteId: string }) {
 
     // TODO 
     // implement windCalculations (cross/headtail/quad/cFactor/cType)
-    // return text value of windState
 
     return (
         <HoverCard>
             <HoverCardTrigger asChild>
-                <div className={`
-                    ${getStatusColor(siteWeather.status, siteWeather.windState)}
-                    shadow-md rounded-2xl w-full p-3 text-xs`}>
+                <div className="shadow-md rounded-2xl w-full p-3 text-xs bg-gray-400">
                     <div className="flex justify-between mx-3">
                         <span className="font-bold text-sm">{siteWeather.siteId}</span>
-                        <span>{formatUnixToTime(siteWeather.time)}</span>
-                        <span className="px-2 py-0.5 mb-1 rounded bg-muted">
+                        <span className="mt-0.5">{formatUnixToTime(siteWeather.time)}</span>
+                        <span className={`
+                            ${getReceivingStatusColor(siteWeather.status)}
+                            px-2 py-0.5 mb-1 rounded-full flex w-30 
+                            justify-center text-white`}>
+                            <RadioTowerIcon className="h-4 w-4 mr-2 text-white" />
                             {formatReceivingStatusText(siteWeather.status)}
                         </span>
                     </div>
-                    <div className="flex gap-2 justify-end">
-                        {/* <WindIcon className="h-4 w-4" /> */}
-                        <span>{siteWeather.windFull} mph</span>
-                        <span>{siteWeather.windDir}° N</span>
-                    </div>
-                    <div className="flex items-center gap-4 justify-end">
-                        
-                        <span>→ {siteWeather.cross.toFixed(1)} mph</span>
-                        <span>↑ {siteWeather.headTail.toFixed(1)} mph</span>
+                    <div className="mt-1 flex justify-end gap-3">
+                        <div
+                            className={`
+                                ${getWindStateColor(siteWeather.windState)}
+                                flex h-8 w-8 shrink-0 items-center
+                                justify-center rounded-full
+                            `}>
+                            <CloudRainWindIcon className="h-5 w-5 text-white" />
+                        </div>
+                        <div className="flex flex-col items-end">
+                            <div className="flex gap-2">
+                                <span>{siteWeather.windFull} mph</span>
+                                <span>{siteWeather.windDir}° N</span>
+                            </div>
+                            <div className="flex gap-2">
+                                <span>→ {siteWeather.cross.toFixed(1)} mph</span>
+                                <span>↑ {siteWeather.headTail.toFixed(1)} mph</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </HoverCardTrigger>
