447e77c272996cbafeb4ef4f6ea371d381c4999a
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-06-30 11:27:34 -0700

    moved public function toward top of file

--- src/features/incoming-weather/core/windCalculations.ts
+++ src/features/incoming-weather/core/windCalculations.ts
@@ -18,6 +18,30 @@ export function getWindCalculations(gunDegrees: number, windFull: number, windTo
     return calcs
 }
 
+
+/**
+ * Returns the wind warning state when compared to set maximum wind and
+ * warning threshold.
+ * 
+ * @param c WindCalcs containing cross and headTail components of the passed
+ * wind reading.
+ * @param maxW The maximum wind allowed compared to total cross or head/tail 
+ * wind components.
+ * @param thresh The percentage threshold (ex .75) where the user wants to
+ * be warned if approaching the max wind.
+ * @returns WindState either Ok, Warn or Critical
+ */
+export function getWindState(c: WindCalcs, maxW: number, thresh: number) 
+    : WindState {
+    if (c.cross > maxW || c.headTail > maxW) {
+        return WindState.Critical;
+    } else if (c.cross > maxW * thresh || c.headTail > maxW * thresh) {
+        return WindState.Warn;
+    } else {
+        return WindState.Ok;
+    }
+}
+
 /*
 DEGREE / QUADRANT NORMALIZATION NOTE
 ------------------------------------
@@ -96,28 +120,7 @@ export function calc(gD: number, wf: number, wT: number) {
     alert(_ht);
 }
 
-/**
- * Returns the wind warning state when compared to set maximum wind and
- * warning threshold.
- * 
- * @param c WindCalcs containing cross and headTail components of the passed
- * wind reading.
- * @param maxW The maximum wind allowed compared to total cross or head/tail 
- * wind components.
- * @param thresh The percentage threshold (ex .75) where the user wants to
- * be warned if approaching the max wind.
- * @returns WindState either Ok, Warn or Critical
- */
-export function getWindState(c: WindCalcs, maxW: number, thresh: number) 
-    : WindState {
-    if (c.cross > maxW || c.headTail > maxW) {
-        return WindState.Critical;
-    } else if (c.cross > maxW * thresh || c.headTail > maxW * thresh) {
-        return WindState.Warn;
-    } else {
-        return WindState.Ok;
-    }
-}
+
 
 
 function getQuadrant(relativeOffset: number): Quadrant {
