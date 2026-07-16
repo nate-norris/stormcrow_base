edf5ae8ce78c064f49f120259368b3c5573be9dd
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-07-16 09:49:29 -0700

    changed padding to increase space below chart

--- src/features/wind-log/components/WindChart.tsx
+++ src/features/wind-log/components/WindChart.tsx
@@ -4,7 +4,7 @@ import { Chart } from "./Chart";
 export function WindChart() {
   return (
     <div className="w-full h-full p-4">
-      <div className="w-full h-full rounded-lg border-2 border-zinc-600 bg-black/10">
+      <div className="w-full h-full rounded-lg border-2 border-zinc-600 bg-black/10 pt-2 px-2">
         <Chart />
       </div>
     </div>
