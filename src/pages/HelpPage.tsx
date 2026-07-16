7c8a2467d7f9a8978899175dd4452baacb12bba6
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-07-16 06:03:11 -0700

    removed useEffect to source help markdown page

--- src/pages/HelpPage.tsx
+++ src/pages/HelpPage.tsx
@@ -1,18 +1,10 @@
-import { useEffect, useState } from "react";
+import helpContent from "@/docs/help.md?raw";
 import ReactMarkdown from "react-markdown";
 
 export default function HelpPage() {
-  const [content, setContent] = useState("");
-
-  useEffect(() => {
-    fetch("../docs/help.md")
-      .then((res) => res.text())
-      .then(setContent);
-  }, []);
-
   return (
     <div className="prose">
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown>{helpContent}</ReactMarkdown>
     </div>
   );
 }
