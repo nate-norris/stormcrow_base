1d74d7f7a85eb212f684ed240929bb96d4566a77
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-07-16 04:53:58 -0700

    WindLog store array of WindEvent

--- src/features/wind-log/state/windLogAtom.ts
+++ src/features/wind-log/state/windLogAtom.ts
@@ -2,5 +2,5 @@ import { atom } from 'jotai'
 
 import type { WindEvent } from "../core/windEvent";
 
-export type WindLog = Record<string, WindEvent>;
+export type WindLog = Record<string, WindEvent[]>;
 export const windLogAtom = atom<WindLog>({});
