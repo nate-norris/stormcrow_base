6665a8d26ab82c558200b99afa8fd1c0fa9e14c7
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-07-16 07:23:52 -0700

    changed atom return value from Map to array since no longer index time

--- src/features/wind-log/state/windChartDataAtom.ts
+++ src/features/wind-log/state/windChartDataAtom.ts
@@ -8,23 +8,18 @@ const WIND_WINDOW = 3 * 60 * 1000;
 export const windChartDataAtom = atom((get) => {
     const windLog = get(windLogAtom);
     const cutoff = get(clockAtom) - WIND_WINDOW;
-    const points = new Map<number, Record<string, number>>();
+    const points: Array<Record<string, number>> = [];
 
     for (const [siteId, events] of Object.entries(windLog)) {
         for (const event of events) {
             if (event.time < cutoff) continue;
 
-            const point = points.get(event.time) ?? {
-                time: event.time
-            };
-
-            point[siteId] = event.windFull;
-
-            points.set(event.time, point);
+            points.push({
+                time: event.time,
+                [siteId]: event.windFull,
+            });
         }
     }
-    console.log("POINT RECEIVED");
-    console.log(points);
-    return Array.from(points.values())
-        .sort((a, b) => a.time - b.time);
+
+    return points.sort((a, b) => a.time - b.time);
 });
