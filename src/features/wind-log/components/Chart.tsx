8e7157c52a73403f83570754f5789ee619a478e7
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-07-16 08:08:02 -0700

    working smoother redraw animation

--- src/features/wind-log/components/Chart.tsx
+++ src/features/wind-log/components/Chart.tsx
@@ -3,6 +3,7 @@ import { ResponsiveContainer, LineChart, XAxis, YAxis, Line } from "recharts";
 
 import { windChartDataAtom } from "../state/windChartDataAtom";
 import { siteIdsAtom } from "@/features/incoming-weather";
+// import { WIND_WINDOW } from "../core/constants";
 
 export function Chart() {
     const siteIds = useAtomValue(siteIdsAtom);
@@ -11,7 +12,14 @@ export function Chart() {
     return (
         <ResponsiveContainer width="100%" height="100%">
             <LineChart data={data}>
-                <XAxis dataKey="time" />
+                <XAxis 
+                    dataKey="time"
+                    type="number"
+                    // domain={[
+                    //     now - WIND_WINDOW,
+                    //     now
+                    // ]}
+                />
                 <YAxis />
 
                 {siteIds.map(siteId => (
@@ -19,7 +27,9 @@ export function Chart() {
                         key={siteId}
                         dataKey={siteId}
                         type="linear"
+                        dot={false}
                         connectNulls={false}
+                        isAnimationActive={false}
                     />
                 ))}
             </LineChart>
