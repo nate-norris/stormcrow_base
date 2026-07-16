c1efa03ef0c1225cc52ba20d822e99b3a58164ff
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-07-16 08:15:51 -0700

    debug: lock on x axis

--- src/features/wind-log/components/Chart.tsx
+++ src/features/wind-log/components/Chart.tsx
@@ -14,7 +14,7 @@ export function Chart() {
             <LineChart data={data}>
                 <XAxis 
                     dataKey="time"
-                    type="number"
+                    // type="number"
                     // domain={[
                     //     now - WIND_WINDOW,
                     //     now
@@ -27,7 +27,7 @@ export function Chart() {
                         key={siteId}
                         dataKey={siteId}
                         type="linear"
-                        dot={false}
+                        // dot={false}
                         connectNulls={false}
                         // isAnimationActive={false}
                     />
