234adcdb45e39f3dd4a75c1dba1f1d0add1b955c
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-07-20 10:51:30 -0700

    update to grid layout first column

--- src/features/qe-logging/components/QEForm.tsx
+++ src/features/qe-logging/components/QEForm.tsx
@@ -13,10 +13,10 @@ export function QEForm() {
 
     return (
         <div className="rounded-lg bg-card text-card-foreground p-6 shadow-md will-change-transform">
-            <h2 className="text-center text-lg font-semibold">Current QE</h2>
+            <h2 className="text-center text-lg font-semibold pb-2">Current QE</h2>
             <FieldSet className="gap-3">
                 <FieldGroup className="gap-3">
-                    <div className="grid grid-cols-3 gap-12">
+                    <div className="grid grid-cols-[auto_1fr] gap-12">
                         <Field>
                             <FieldLabel htmlFor="qe-dodic" className="ml-auto">DODIC</FieldLabel>
                             <DodicInput />
@@ -27,7 +27,7 @@ export function QEForm() {
                         </Field>
                     </div>
                     <Separator />
-                    <div className="grid grid-cols-3 gap-12">
+                    <div className="grid grid-cols-[auto_1fr] gap-12">
                         <Field>
                             <FieldLabel htmlFor="qe-count" >QE</FieldLabel>
                             <QECountSpinner />
