3fe71c0d5c7c01398d393afaa6eb4adef2f9a340
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-08-12 09:00:25 -0700

    adding cross and headTail to tracked attributes

--- src/features/wind-log/core/windEvent.ts
+++ src/features/wind-log/core/windEvent.ts
@@ -1,6 +1,7 @@
-// TODO: possibly translate this to accept the highest of cross or headtail so it is clearer when thresholds are passed
 // structure for past log of wind values
 export type WindEvent = {
     time: number,
-    windFull: number
+    windFull: number,
+    cross: number,
+    headTail: number,
 }
