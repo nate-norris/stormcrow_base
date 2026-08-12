c328c3cfc8bde3d8f90a6752ab1eb61f3fcad989
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-08-12 13:09:38 -0700

    swapped wind full to cross/head tail charts

--- src/features/wind-log/components/WindChart.tsx
+++ src/features/wind-log/components/WindChart.tsx
@@ -1,12 +1,30 @@
-
+import { useAtomValue } from "jotai";
 import { Chart } from "./Chart";
+import { crossWindChartDataAtom, headTailWindChartDataAtom } from "../state/windChartDataAtom";
+import { ChartLegend } from "./ChartLegend";
 
 export function WindChart() {
+  const crossData = useAtomValue(crossWindChartDataAtom);
+  const headTailData = useAtomValue(headTailWindChartDataAtom);
+
   return (
     <div className="w-full h-full p-4">
-      <div className="w-full h-full rounded-lg shadow-md shadow-black/30 bg-black/10 pt-2 px-2">
-        <Chart />
+      <div className="w-full h-full flex shadow-md shadow-black/30 rounded-lg bg-black/10 overflow-hidden">
+        
+        <div className="flex-1 min-w-0 min-h-0 flex flex-col">
+          <div className="flex-1 min-h-0 pt-2 px-2">
+            <Chart data={crossData} label={"Cross (m/s)"} />
+          </div>
+
+          <div className="flex-1 min-h-0 pt-2 px-2">
+            <Chart data={headTailData} label={"HeadTail (m/s)"}/>
+          </div>
+        </div>
+
+        <div className="w-14 min-h-0">
+          <ChartLegend />
+        </div>
       </div>
     </div>
-  )
+  );
 }
