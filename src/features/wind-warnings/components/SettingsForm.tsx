6c20c5e63418800481aec4b374845bebff16e752
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-08-13 11:30:06 -0700

    correct weapon orientation minimum direction to 0

--- src/features/wind-warnings/components/SettingsForm.tsx
+++ src/features/wind-warnings/components/SettingsForm.tsx
@@ -13,7 +13,7 @@ import { persistWindWarningConfig } from "@/tauri";
 const CONFIG_LIMITS = {
   maxWind: { min: 0, max: 20 },
   percentage: { min: 0, max: 100 },
-  direction: { min: 1, max: 359 },
+  direction: { min: 0, max: 359 },
   siteCount: { min: 1, max: 26 }, // A-Z
 };
 
