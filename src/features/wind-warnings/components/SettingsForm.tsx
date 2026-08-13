29f3c1f0ccb338e435dfc23951c501d320dbae9f
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-08-13 11:27:12 -0700

    added validation of config values before submit

--- src/features/wind-warnings/components/SettingsForm.tsx
+++ src/features/wind-warnings/components/SettingsForm.tsx
@@ -1,24 +1,78 @@
 import { useState, useEffect } from "react";
-import { useAtom } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { toast } from "sonner";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FieldGroup, Field, FieldLabel } from "@/components/ui/field";
+import type { WindWarningConfig } from "../core/models";
 import { activeWindConfigAtom } from "../state/windWarnAtom";
+import { canUpdateConfigsAtom } from "../state/canUpdateConfigsAtom";
 import { persistWindWarningConfig } from "@/tauri";
 
+const CONFIG_LIMITS = {
+  maxWind: { min: 0, max: 20 },
+  percentage: { min: 0, max: 100 },
+  direction: { min: 1, max: 359 },
+  siteCount: { min: 1, max: 26 }, // A-Z
+};
+
 export default function WindWarningForm() {
+  const canUpdateConfigs = useAtomValue(canUpdateConfigsAtom);
   // global wind warning config atom
   const [activeWindConfig, setActiveWindConfig] = useAtom(activeWindConfigAtom);
   // pre-loaded config for local form values
   const [draftConfig, setDraftConfig] = useState(activeWindConfig);
+  // display any errors upon input
+  const [formError, setFormError] = useState<string>("");
 
   useEffect(() => {
     setDraftConfig(activeWindConfig);
   }, [activeWindConfig]);
 
+  function validateConfig(config: WindWarningConfig): string {
+    alert(config);
+    // confirm max wind
+    if (config.maxWind < CONFIG_LIMITS.maxWind.min || 
+      config.maxWind > CONFIG_LIMITS.maxWind.max) {
+      return `Max wind must be between ${CONFIG_LIMITS.maxWind.min} and 
+        ${CONFIG_LIMITS.maxWind.max} m/s.`;
+    }
+
+    // confirm threshold percent
+    if (config.thresholdPercent < CONFIG_LIMITS.percentage.min || 
+      config.thresholdPercent > CONFIG_LIMITS.percentage.max) {
+      return `Threshold must be between ${CONFIG_LIMITS.percentage.min} and 
+        ${CONFIG_LIMITS.percentage.max} %.`;
+    }
+
+    // confirm weapon orientation
+    if (config.gunOrient < CONFIG_LIMITS.direction.min || 
+      config.gunOrient > CONFIG_LIMITS.direction.max) {
+      return `Weapon direction must be between ${CONFIG_LIMITS.direction.min}° and 
+        ${CONFIG_LIMITS.direction.max}°.`;
+    }
+  
+    // confirm site count
+    if (config.expectedSites < CONFIG_LIMITS.siteCount.min || 
+      config.expectedSites > CONFIG_LIMITS.siteCount.max) {
+      return `Site count must be between ${CONFIG_LIMITS.siteCount.min} and 
+        ${CONFIG_LIMITS.siteCount.max}.`;
+    }
+
+    return "";
+  }
+
   const handleUpdate = async () => {
+
+    // update ui for any error messages
+    //  return if one is present
+    const error = validateConfig(draftConfig);
+    setFormError(error);
+    if (error != "") {
+      return;
+    }
+
     try {
       await persistWindWarningConfig(draftConfig);
       setActiveWindConfig(draftConfig);
@@ -35,11 +89,13 @@ export default function WindWarningForm() {
       <form>
         <FieldGroup>
           <Field className="flex items-center gap-4 flex-row">
-            {/* TODO: enforce positive */}
+            {/* max wind speed */}
             <FieldLabel htmlFor="max-ws">Max Wind Speed (m/s)</FieldLabel>
             <Input 
               id="max-ws"
               type="number"
+              min={CONFIG_LIMITS.maxWind.min}
+              max={CONFIG_LIMITS.maxWind.max}
               step="1"
               value={draftConfig.maxWind}
               onChange={(e) =>
@@ -51,12 +107,14 @@ export default function WindWarningForm() {
               className="w-25 bg-input text-foreground"
             />
           </Field>
-          {/* TODO: 0-100 enforced; may need to be above 0 */}
+          {/* threshold percent of warning up to max */}
           <Field className="flex items-center gap-4 flex-row">
             <FieldLabel htmlFor="threshold-ps">Threshold (%)</FieldLabel>
             <Input
               id="threshold-ps"
               type="number"
+              min={CONFIG_LIMITS.percentage.min}
+              max={CONFIG_LIMITS.percentage.max}
               step="5"
               value={draftConfig.thresholdPercent}
               onChange={(e) =>
@@ -68,12 +126,14 @@ export default function WindWarningForm() {
               className="w-25 bg-input text-foreground"
             />
           </Field>
-          {/* TODO: 0 - 359 enforced */}
+          {/* direction for wind degrees */}
           <Field className="flex items-center gap-4 flex-row">
             <FieldLabel htmlFor="weapon-deg">Weapon (° N True)</FieldLabel>
             <Input
               id="weapon-deg"
               type="number"
+              min={CONFIG_LIMITS.direction.min}
+              max={CONFIG_LIMITS.direction.max}
               step="1"
               value={draftConfig.gunOrient}
               onChange={(e) =>
@@ -85,11 +145,14 @@ export default function WindWarningForm() {
               className="w-25 bg-input text-foreground"
             />
           </Field>
+          {/* number of expected sites receiving */}
           <Field className="flex items-center gap-4 flex-row">
             <FieldLabel htmlFor="exp-sites">Expected Sites</FieldLabel>
             <Input
               id="exp-sites"
               type="number"
+              min={CONFIG_LIMITS.siteCount.min}
+              max={CONFIG_LIMITS.siteCount.max}
               step="1"
               value={draftConfig.expectedSites}
               onChange={(e) =>
@@ -102,7 +165,10 @@ export default function WindWarningForm() {
             />
           </Field>
           <div className="flex ml-auto">
+            <span className="text-destructive">{formError}</span>
             <Button
+              type="button"
+              disabled={!canUpdateConfigs}
               onClick={handleUpdate}>
               Update
             </Button>
