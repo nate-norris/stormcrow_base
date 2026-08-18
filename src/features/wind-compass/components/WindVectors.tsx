f6e6cd71ae28523ae2f46acc8c83e6951139e6a9
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-08-18 12:20:36 -0700

    changed tab sizing

--- src/features/wind-compass/components/WindVectors.tsx
+++ src/features/wind-compass/components/WindVectors.tsx
@@ -4,21 +4,21 @@ import { compassDataAtom } from "../state/compassAtom";
 import { WindVector } from "./WindVector";
 
 interface WindVectorProps {
-    radius: number;
+  radius: number;
 }
 
 export function WindVectors({ radius }: WindVectorProps) {
-    const compassEvents = useAtomValue(compassDataAtom);
+  const compassEvents = useAtomValue(compassDataAtom);
 
-    return (
-        <>
-            {Object.entries(compassEvents).map(([siteId, compassEvent]) => (
-                <WindVector
-                    siteId={siteId}
-                    event={compassEvent}
-                    radius={radius}
-                />
-            ))}
-        </>
-    );
+  return (
+    <>
+      {Object.entries(compassEvents).map(([siteId, compassEvent]) => (
+        <WindVector
+          siteId={siteId}
+          event={compassEvent}
+          radius={radius}
+        />
+      ))}
+    </>
+  );
 }
