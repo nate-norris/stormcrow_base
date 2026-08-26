a286159a274712516b36ea28e639a08a9af50e26
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-08-26 08:34:21 -0700

    modified layout of weather site skeleton

--- src/features/incoming-weather/components/SkeletonItem.tsx
+++ src/features/incoming-weather/components/SkeletonItem.tsx
@@ -2,12 +2,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export function SkeletonWeather() {
   return (
-    <div className="flex items-center gap-4">
-      <Skeleton className="h-10 w-10 rounded-full" />
-      <div className="space-y-2">
-        <Skeleton className="h-3 w-[120px]" />
-        <Skeleton className="h-3 w-[160px]" />
-        <Skeleton className="h-3 w-[120px]" />
+    <div className="flex items-center gap-4 ml-10">
+      <Skeleton className="h-10 w-10 rounded-full shrink-0" />
+      <div className="space-y-2 w-full">
+        <Skeleton className="h-3 w-2/3" />
+        <Skeleton className="h-3 w-full" />
+        <Skeleton className="h-3 w-2/3" />
       </div>
     </div>
   );
