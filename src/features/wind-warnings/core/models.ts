48863a868cd8d6688ba88d5fedc80ad5bdafb63c
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-08-27 11:35:34 -0700

    pulled out location features

--- src/features/wind-warnings/core/models.ts
+++ src/features/wind-warnings/core/models.ts
@@ -1,5 +1,3 @@
-import LatLonVincenty from 'geodesy/latlon-ellipsoidal-vincenty.js';
-
 /**
  * Represents the settings the user will provide for real-time wind comparison.
  * @interface WindWarningSettings
@@ -12,43 +10,3 @@ export interface WindWarningConfig {
   expectedSites: number;
 }
 
-export type Hemisphere = "N" | "S";
-
-type Coordinate = {
-  easting: number;
-  northing: number;
-}
-
-// input for weapon/target after validation
-export type AzimuthInput = {
-  utm: number;
-  hemisphere: Hemisphere;
-  point: Coordinate;
-}
-
-// non corrected inputs
-export type AzimuthRawInput = {
-  utm: number;
-  hemisphere: Hemisphere;
-  easting: string;
-  northing: string;
-}
-
-export function toInput(raw: AzimuthRawInput): AzimuthInput {
-  return {
-    utm: raw.utm,
-    hemisphere: raw.hemisphere,
-    point: {
-      easting: Number(raw.easting),
-      northing: Number(raw.northing)
-    }
-  }
-}
-
-export type AzimuthOutput = {
-  weapon: LatLonVincenty;
-  target: LatLonVincenty;
-  weaponElevation: number;
-  targetElevation: number;
-  azimuth: number;
-}
