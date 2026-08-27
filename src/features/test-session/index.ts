11cddfb2f6bdc9e3dfeab30523650384d66fbac4
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-08-27 08:17:01 -0700

    export LastTest for tauri commands

--- src/features/test-session/index.ts
+++ src/features/test-session/index.ts
@@ -1,4 +1,4 @@
 export { default as TestModal } from "./components/TestModal";
 export { activeTestAtom } from "./state/testAtom";
-export type { Test, TestSession } from "./core/models";
+export type { Test, TestSession, LastTest } from "./core/models";
 export { EmptyTestSelectionPrompt } from "./components/EmptyTestSelectionPrompt";
