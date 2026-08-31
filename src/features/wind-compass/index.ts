b2cf2aed66bd7a27eabc0cb860b37696134b788f
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-08-31 07:08:12 -0700

    export needed component/types for menu

--- src/features/wind-compass/index.ts
+++ src/features/wind-compass/index.ts
@@ -1,3 +1,5 @@
 export { WindCompass } from "./components/WindCompass";
 export { updateCompassAtom } from "./state/updateCompassAtom";
 export { deleteCompassAtom } from "./state/deleteCompassAtom";
+export { weaponSelectionAtom } from "./state/selectedWeaponAtom";
+export { WEAPONS, type WeaponKey } from "./core/weaponKeys";
