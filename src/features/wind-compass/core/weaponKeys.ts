f7b145338cfe95f961a7e5a5aef804cc246c5a9a
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-08-31 07:07:05 -0700

    Record weapon key to radio menu string

---
+++ src/features/wind-compass/core/weaponKeys.ts
@@ -0,0 +1,9 @@
+export const WEAPONS = {
+    hk416: "HK416",
+    m249: "M249",
+    shnipes: "Shnipes",
+    ma5b: "MA5B",
+    sword: "Energy Sword",
+} as const;
+
+export type WeaponKey = keyof typeof WEAPONS;
