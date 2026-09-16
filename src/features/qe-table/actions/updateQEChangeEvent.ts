0d482614acb35aba498a7bad4f64d39e97c0cb4a
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-16 15:48:28 -0700

    change events now through qe feature

--- src/features/qe-table/actions/updateQEChangeEvent.ts
+++
@@ -1,8 +0,0 @@
-import { store } from "@/state/store"
-import { qeChangeAtom } from "../state/qeChangeAtom";
-import { QEChange } from "../core/qeChange";
-
-export function updateQEChangeEvent(change: QEChange) {
-    // add QEChange event
-  store.set(qeChangeAtom, change);
-}
