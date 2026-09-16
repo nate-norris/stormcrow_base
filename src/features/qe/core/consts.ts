d3fad884061097e3aea9d879a27a7fa638ad97b5
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-16 16:41:47 -0700

    aded commands and event consts

--- src/features/qe/core/consts.ts
+++ src/features/qe/core/consts.ts
@@ -2,3 +2,15 @@ export const QE_TYPES = {
   TR: "Test Round (TR)",
   WS: "Warmer Spotter (WS)",
 } as const;
+
+export const QE_COMMANDS = {
+  CREATE: "create",
+  DELETE: "delete",
+  REASSIGN: "reassign",
+} as const;
+
+export const QE_EVENTS = {
+  CREATED: "created",
+  DELETED: "deleted",
+  REASSIGNED: "reassigned",
+} as const;
