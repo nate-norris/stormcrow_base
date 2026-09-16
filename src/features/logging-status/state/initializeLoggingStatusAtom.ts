373f0610b9048fe062a651077631c1d68776539c
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-16 16:00:52 -0700

    QEType from qe feature

--- src/features/logging-status/state/initializeLoggingStatusAtom.ts
+++ src/features/logging-status/state/initializeLoggingStatusAtom.ts
@@ -2,7 +2,7 @@ import { atom } from "jotai";
 
 import { loggingStatusAtom } from "./loggingStatusAtom";
 import { type WeatherRow } from "@/features/qe-table";
-import { type QEType } from "@/features/qe-tracking";
+import type { QEType } from "@/features/qe";
 
 export const initializeLoggingStatusAtom = atom(
     null,
