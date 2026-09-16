e110df59ce52c987c26ae80639ebb36c178b3ff9
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-16 16:50:50 -0700

    handled through different file/function

--- src/tauri/commands/qe/persistQE.ts
+++
@@ -1,8 +0,0 @@
-import { invoke } from "@tauri-apps/api/core";
-
-import { QEEntry } from "@/features/qe-logging/";
-import type { WeatherRow } from "@/features/qe-table";
-
-export async function dbPersistQEEntry(entry: QEEntry): Promise<WeatherRow[]> {
-  return await invoke<WeatherRow[]>("insert_new_qe_command", { newQe: entry });
-}
