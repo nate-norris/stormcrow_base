ff0f277b0890346314f8fcda618ab7056ecb90ae
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-16 15:24:35 -0700

    qe count and type current tracked through qe domain and not form

--- src/features/logging-status/state/updateOnLoggingStatusAtom.ts
+++
@@ -1,8 +0,0 @@
-// export const loggingStatusAtom = atom<LoggingStatus>((get): LoggingStatus => {
-//     const qeForm = get(activeQEFormAtom);
-
-//     return {
-//         qeCount: qeForm.qeCount,
-//         qeType: qeForm.qeType,
-//     };
-// });
