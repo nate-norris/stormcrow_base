c0977825b8f4da51e9cfabb19b10c0373a00bd2f
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-16 16:14:31 -0700

    removed QEKey verbage

--- src/features/qe-table/actions/addSingleQETableRows.ts
+++ src/features/qe-table/actions/addSingleQETableRows.ts
@@ -1,25 +1,26 @@
 import { store } from "@/state/store"
 import { weatherRowsAtom } from "../state/weatherRowsAtom";
-import type { QEType, QEKey } from "@/features/qe-tracking";
+import type { QEType, QE } from "@/features/qe";
 import type { WeatherRow } from "../core/weatherRow";
-import { rowsNotSpecifiedByKey } from "../actions/rowsNotByKey";
+import { rowsNotSpecifiedByQE } from "./rowsNotSpecifiedByQE";
 
-export function replaceQERows(newRows: WeatherRow[]) {
-    // no qe to delete
+export function addSingleQETableRows(newRows: WeatherRow[]) {
+    // no qe to be added
     if (newRows.length === 0) return;
-    // confirm no mixing of QEs and has same QEKey
+
+    // confirm no mixing of QEs and has same count/type
     if (!isSingleQE(newRows)) 
-        throw new Error("Test selection required to log QEs.");
+        throw new Error("Attempted replace of more than one QE.");
     
     // add to table row source
-    const key: QEKey = {
+    const qe: QE = {
         count: newRows[0].count,
         qeType: newRows[0].qeType as QEType,
     }
 
-    // drop any previous rows at QEKey and add new rows
+    // drop any previous rows at QE and add new rows
     store.set(weatherRowsAtom, [
-      ...rowsNotSpecifiedByKey(store.get(weatherRowsAtom), key),
+      ...rowsNotSpecifiedByQE(store.get(weatherRowsAtom), qe),
       ...newRows,
     ]);
 }
