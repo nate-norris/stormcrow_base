d5249323c5ab5e2c85845a0620e76244fba4212f
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-16 15:48:01 -0700

    not needed

--- src/features/qe-logging/services/resolveQEForm.ts
+++
@@ -1,7 +0,0 @@
-// export function resolveQEFormState(
-//   mode: "init" | "reset" | "next",
-//   last?: QELog
-// ): QEFormState
-// TODO: add universal entry to update form state. 
-// would account for new/continue test, user update qe, and update qe when firing
-// remove resetForm/updateForm functions and only expose this interface
