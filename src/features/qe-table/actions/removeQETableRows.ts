c989dd511811116c5dc30a27a4730a51dd1ad870
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-16 16:14:52 -0700

    added docstring

--- src/features/qe-table/actions/removeQETableRows.ts
+++ src/features/qe-table/actions/removeQETableRows.ts
@@ -4,6 +4,12 @@ import type { QE } from "@/features/qe";
 import { weatherRowsAtom } from "../state/weatherRowsAtom";
 import { rowsNotSpecifiedByQE } from "./rowsNotSpecifiedByQE";
 
+/**
+ * Updates weatherRowsAtom to remove all WeatherRows not
+ * specified by the provided QE.
+ * 
+ * @param qe 
+ */
 export function removeQETableRows(qe: QE) {
   // remove from weather rows
   store.set(weatherRowsAtom,
@@ -13,4 +19,3 @@ export function removeQETableRows(qe: QE) {
     )
   );
 }
-
