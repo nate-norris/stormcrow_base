36d3c06826d447fc158ee8b58fdd106873d65999
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-16 16:53:51 -0700

    added test atom verification and boolean return type of reassign

--- src/tauri/commands/qe/reassignQEDatabase.ts
+++ src/tauri/commands/qe/reassignQEDatabase.ts
@@ -1,14 +1,32 @@
 import { invoke } from "@tauri-apps/api/core";
 
-import { QEBase } from "@/features/qe-logging";
+import { store } from "@/state";
+import { activeTestAtom } from "@/features/test-session";
+import { QE } from "@/features/qe";
 import { WeatherRow } from "@/features/qe-table";
 
-export async function reassignQEDatabase(source: QEBase, destination: QEBase)
-    : Promise<WeatherRow[]> {
+import { QEBase } from "./types";
 
-    // get new rows from database entry
-    return await invoke<WeatherRow[]>("reassign_qe_command", {
-        source: source, 
-        destination: destination
-    });
+export async function reassignQEDatabase(source: QE, destination: QE)
+  : Promise<WeatherRow[] | null> {
+  const test = store.get(activeTestAtom);
+
+  if (!test) return null;
+
+  const sourceBase: QEBase = {
+    count: source.count,
+    qeType: source.qeType,
+    testId: test.id,
+  };
+  const destinationBase: QEBase = {
+    count: destination.count,
+    qeType: destination.qeType,
+    testId: test.id,
+  };
+
+  // get new rows from database entry
+  return await invoke<WeatherRow[]>("reassign_qe_command", {
+    source: sourceBase, 
+    destination: destinationBase
+  });
 }
