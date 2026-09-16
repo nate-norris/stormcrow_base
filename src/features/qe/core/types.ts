b9c665d6234fa0cb032af9dfb6acef6f77206d10
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-16 16:42:25 -0700

    added LoggingFormState, QECommand types and QEEvent types

--- src/features/qe/core/types.ts
+++ src/features/qe/core/types.ts
@@ -1,22 +1,46 @@
-import { QE_TYPES } from "./consts";
+import { type WeatherRow } from "@/features/qe-table";
+
+import { QE_TYPES, QE_COMMANDS, QE_EVENTS } from "./consts";
 
 export type QEType = keyof typeof QE_TYPES;
 
-export type QEKey = {
+export type QE = {
   count: number;
   qeType: QEType;
 };
 
-export type QEChange =
-    | {
-        type: "deleted";
-        key: QEKey;
-    }
-    | {
-        type: "reassigned";
-        oldKey: QEKey;
-        newKey: QEKey;
-    } | {
-        type: "new";
-        key: QEKey;
-    };
+export type LoggingFormState = {
+    dodic: string;
+    lot: string;
+    qe: QE
+};
+
+export type QECommand =
+| {
+    type: typeof QE_COMMANDS.CREATE;
+    loggingForm: LoggingFormState;
+  }
+  | {
+    type: typeof QE_COMMANDS.DELETE;
+    qe: QE;
+  } | {
+    type: typeof QE_COMMANDS.REASSIGN;
+    base: QE;
+    destination: QE;
+  };
+
+export type QEEvent = 
+  {
+    type: typeof QE_EVENTS.CREATED;
+    qe: QE;
+    newTableRows: WeatherRow[];
+  } |
+  {
+    type: typeof QE_EVENTS.DELETED;
+    qe: QE;
+  } | {
+    type: typeof QE_EVENTS.REASSIGNED;
+    base: QE;
+    destination: QE;
+    newTableRows: WeatherRow[];
+  }
