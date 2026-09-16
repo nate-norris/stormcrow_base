8d0991ab8b4ff55ca630c0758ca1d07c3e818836
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-16 16:43:13 -0700

    export qe feature types and commands

--- src/features/qe/index.ts
+++ src/features/qe/index.ts
@@ -1,9 +1,12 @@
 // types and consts
-export type { QEType, QE } from "./core/types";
-export { QE_TYPES } from "./core/consts";
+export type { QEType, QE, QECommand, QEEvent, LoggingFormState } 
+    from "./core/types";
+export { QE_TYPES, QE_COMMANDS, QE_EVENTS } from "./core/consts";
 
 // api functions
-export { qeFromString } from "./services/qeKeyFromString";
+export { qeFromString } from "./services/qeFromString";
+export { processQECommand } from "./services/processQECommand";
+export { subscribeToQEEvent } from "./services/subscribe";
 
 // components
 export { CountSpinner as QECountSpinner } from "./components/CountSpinner";
@@ -13,3 +16,4 @@ export { ActiveTypeSelector as ActiveQETypeSelector } from "./components/ActiveT
 
 // state
 export { activeQEAtom } from "./state/activeQEAtom";
+export { qeCountAtom } from "./state/qeCountAtom";
