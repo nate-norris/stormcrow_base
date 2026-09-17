f561f0624915b17445479514e0ee30b5917004f3
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-17 08:54:42 -0700

    added LoggingStatusView to qe table tab

--- src/components/layouts/QETableView.tsx
+++ src/components/layouts/QETableView.tsx
@@ -1,10 +1,12 @@
 import { QETable } from "@/features/qe-table";
 import { ExportDataButton } from "@/features/qe-export";
+import { LoggingStatusView } from "@/features/logging-status";
 
 export function QETableView() {
   return (
     <div className="h-full min-h-0 overflow-auto">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex flex-row">
+        <LoggingStatusView />
         <div className="ml-auto w-32">
           <ExportDataButton />
         </div>
