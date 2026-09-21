49952f0b0616addb5750ff3869675f88dc996710
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-09-21 13:10:56 -0700

    specify return type of TauriEventHandler

--- src/tauri/events/models.ts
+++ src/tauri/events/models.ts
@@ -1,7 +1,9 @@
+import type { UnlistenFn } from "@tauri-apps/api/event";
+
 /**
  * Interface for declaring register method to begin awaiting and handling
  * Tauri events.
  */
 export interface TauriEventHandler {
-    register(): Promise<void>;
+    register(): Promise<UnlistenFn>;
 }
